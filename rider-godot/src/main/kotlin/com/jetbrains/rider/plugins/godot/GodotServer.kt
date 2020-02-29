e6dabf0c45acb9e5b581853c06c83d581daef2e9
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-02-29 15:59:12 +0100

    generate different types of configurations

---
+++ rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotServer.kt
@@ -0,0 +1,18 @@
+package com.jetbrains.rider.plugins.godot
+
+import com.intellij.openapi.project.Project
+import com.jetbrains.rider.test.framework.combine
+import java.io.File
+
+class GodotServer {
+    companion object {
+        fun getPath(project: Project): String {
+            val metaFile = File(project.basePath).combine(".mono\\metadata\\ide_server_meta.txt")
+            val lines = metaFile.readLines()
+            if (lines.count()<2)
+                return ""
+
+            return lines[1]
+        }
+    }
+}
--- rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/GodotRunConfigurationGenerator.kt
+++ rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/GodotRunConfigurationGenerator.kt
@@ -5,13 +5,21 @@ import com.intellij.execution.configurations.ConfigurationTypeUtil
 import com.intellij.openapi.project.Project
 import com.jetbrains.rdclient.util.idea.LifetimedProjectComponent
 import com.jetbrains.rider.plugins.godot.GodotProjectDiscoverer
+import com.jetbrains.rider.plugins.godot.GodotServer
+import com.jetbrains.rider.run.configurations.dotNetExe.DotNetExeConfiguration
+import com.jetbrains.rider.run.configurations.dotNetExe.DotNetExeConfigurationType
+import com.jetbrains.rider.run.configurations.exe.ExeConfiguration
+import com.jetbrains.rider.run.configurations.exe.ExeConfigurationType
 import com.jetbrains.rider.run.configurations.remote.DotNetRemoteConfiguration
 import com.jetbrains.rider.run.configurations.remote.MonoRemoteConfigType
 
 class GodotRunConfigurationGenerator(project: Project) : LifetimedProjectComponent(project) {
 
     companion object {
-        const val ATTACH_CONFIGURATION_NAME = "Attach to Godot Player"
+        const val ATTACH_CONFIGURATION_NAME = "Attach to Player"
+        const val DEBUG_CONFIGURATION_NAME = "Debug Player"
+        const val RUN_CONFIGURATION_NAME = "Run Player"
+        const val PROFILE_CONFIGURATION_NAME = "Profile Player"
     }
 
     init {
@@ -19,7 +27,7 @@ class GodotRunConfigurationGenerator(project: Project) : LifetimedProjectCompone
         val runManager = RunManager.getInstance(project)
         if (projectDiscoverer.getIsGodotProject) {
             // Add configuration, if it doesn't exist
-            if (!runManager.allSettings.any { it.type is MonoRemoteConfigType}) {
+            if (!runManager.allSettings.any { it.type is MonoRemoteConfigType && it.name == ATTACH_CONFIGURATION_NAME}) {
                 val configurationType = ConfigurationTypeUtil.findConfigurationType(MonoRemoteConfigType::class.java)
                 val runConfiguration = runManager.createConfiguration(ATTACH_CONFIGURATION_NAME, configurationType.factory)
                 val remoteConfig = runConfiguration.configuration as DotNetRemoteConfiguration
@@ -28,9 +36,39 @@ class GodotRunConfigurationGenerator(project: Project) : LifetimedProjectCompone
                 runManager.addConfiguration(runConfiguration)
             }
 
+            if (!runManager.allSettings.any { it.type is ExeConfigurationType && it.name == RUN_CONFIGURATION_NAME}) {
+                val configurationType = ConfigurationTypeUtil.findConfigurationType(ExeConfigurationType::class.java)
+                val runConfiguration = runManager.createConfiguration(RUN_CONFIGURATION_NAME, configurationType.factory)
+                val config = runConfiguration.configuration as ExeConfiguration
+                config.parameters.exePath = "C:/Windows/System32/cmd.exe"
+                config.parameters.programParameters = "/c start \"\" \"${GodotServer.getPath(project)}\" --path \"${project.basePath}\""
+                config.parameters.workingDirectory = "${project.basePath}"
+                runManager.addConfiguration(runConfiguration)
+            }
+
+            if (!runManager.allSettings.any { it.type is MonoRemoteConfigType && it.name == DEBUG_CONFIGURATION_NAME}) {
+                val configurationType = ConfigurationTypeUtil.findConfigurationType(MonoRemoteConfigType::class.java)
+                val runConfiguration = runManager.createConfiguration(DEBUG_CONFIGURATION_NAME, configurationType.factory)
+                val remoteConfig = runConfiguration.configuration as DotNetRemoteConfiguration
+                remoteConfig.port = projectDiscoverer.port
+                // Not shared, as that requires the entire team to have same port
+                runManager.addConfiguration(runConfiguration)
+            }
+
+            if (!runManager.allSettings.any { it.type is DotNetExeConfigurationType && it.name == PROFILE_CONFIGURATION_NAME}) {
+                val configurationType = ConfigurationTypeUtil.findConfigurationType(DotNetExeConfigurationType::class.java)
+                val runConfiguration = runManager.createConfiguration(PROFILE_CONFIGURATION_NAME, configurationType.factory)
+                val config = runConfiguration.configuration as DotNetExeConfiguration
+                config.parameters.exePath = GodotServer.getPath(project)
+                config.parameters.programParameters = "--path \"${project.basePath}\""
+                config.parameters.workingDirectory = "${project.basePath}"
+                config.parameters.useMonoRuntime = true
+                runManager.addConfiguration(runConfiguration)
+            }
+
             // make configuration selected if nothing is selected
             if (runManager.selectedConfiguration == null) {
-                val runConfiguration = runManager.findConfigurationByName(ATTACH_CONFIGURATION_NAME)
+                val runConfiguration = runManager.findConfigurationByName(DEBUG_CONFIGURATION_NAME)
                 if (runConfiguration != null) {
                     runManager.selectedConfiguration = runConfiguration
                 }
