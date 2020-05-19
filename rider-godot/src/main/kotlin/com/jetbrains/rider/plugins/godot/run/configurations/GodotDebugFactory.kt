e805e5c65cca227243252b567556aad205454494
Jiri Sejkora <jiri.sejkora@jetbrains.com>
2020-05-19 15:30:39 +0200

    Use a fixed id for the Start and Debug run configuration
    This removes a DeprecatedMethodException thrown by the default
    implementation of getId() and solves the reason why it's thrown.

--- rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/configurations/GodotDebugFactory.kt
+++ rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/configurations/GodotDebugFactory.kt
@@ -10,4 +10,9 @@ open class GodotDebugFactory(type: ConfigurationType)
     override fun createTemplateConfiguration(project: Project) = GodotDebugRunConfiguration(project, this)
     override fun isConfigurationSingletonByDefault() = true
     override fun getIcon() = GodotIcons.RunConfigurations.StartAndDebug
+
+    // This value gets written to the config file. By default it defers to getName, however,
+    // the value needs to be kept the same even if the display name changes in the future
+    // in order to maintain compatibility with older configs.
+    override fun getId() = "Start and Debug"
 }
