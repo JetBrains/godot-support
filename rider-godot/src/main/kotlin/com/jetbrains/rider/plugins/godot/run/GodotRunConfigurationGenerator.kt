ac19a0c969e9ccd1c8080b9c0e2f509cb5ea6657
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-07-01 23:30:04 +0200

    cleanup old Run and Profile configurations

--- rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/GodotRunConfigurationGenerator.kt
+++ rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/GodotRunConfigurationGenerator.kt
@@ -2,13 +2,14 @@ package com.jetbrains.rider.plugins.godot.run
 
 import com.intellij.execution.RunManager
 import com.intellij.execution.configurations.ConfigurationTypeUtil
-import com.intellij.execution.configurations.UnknownConfigurationType
 import com.intellij.openapi.project.Project
 import com.jetbrains.rdclient.util.idea.LifetimedProjectComponent
 import com.jetbrains.rider.plugins.godot.GodotProjectDiscoverer
 import com.jetbrains.rider.plugins.godot.GodotServer
-import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfigurationType
 import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfiguration
+import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfigurationType
+import com.jetbrains.rider.run.configurations.dotNetExe.DotNetExeConfigurationType
+import com.jetbrains.rider.run.configurations.exe.ExeConfigurationType
 import com.jetbrains.rider.run.configurations.remote.DotNetRemoteConfiguration
 import com.jetbrains.rider.run.configurations.remote.MonoRemoteConfigType
 import java.io.File
@@ -29,6 +30,15 @@ class GodotRunConfigurationGenerator(project: Project) : LifetimedProjectCompone
         if (projectDiscoverer.getIsGodotProject) {
             val runManager = RunManager.getInstance(project)
             val godotPath = File(GodotServer.getGodotPath(project))
+
+            // Clean up old generated configurations
+            val toRemove = runManager.allSettings.filter {
+                it.type is ExeConfigurationType && it.name == RUN_CONFIGURATION_NAME
+                        || it.type is DotNetExeConfigurationType && it.name == PROFILE_CONFIGURATION_NAME
+            }
+            for (value in toRemove) {
+                runManager.removeConfiguration(value)
+            }
 
             // Add configuration, if it doesn't exist
             if (!runManager.allSettings.any { it.type is MonoRemoteConfigType && it.name == ATTACH_CONFIGURATION_NAME }) {
