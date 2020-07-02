b66ded1dc07c80a5416975a82f0f0b295c3d1f3e
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-07-02 09:34:27 +0200

    cleanup

--- rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/configurations/GodotDebugRunFactory.kt
+++ rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/configurations/GodotDebugRunFactory.kt
@@ -1,13 +1,9 @@
-import com.intellij.execution.BeforeRunTask
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
 import com.intellij.execution.configurations.RunConfiguration
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Key
-import com.jetbrains.rider.build.tasks.BuildSolutionBeforeRunTask
-import com.jetbrains.rider.build.tasks.BuildSolutionBeforeRunTaskProvider
-import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfigurationParameters
 import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfiguration
+import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfigurationParameters
 import org.jetbrains.annotations.NotNull
 
 open class GodotDebugRunFactory(type: ConfigurationType) : ConfigurationFactory(type) {
