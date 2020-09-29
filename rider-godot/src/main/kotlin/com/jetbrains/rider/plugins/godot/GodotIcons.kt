13792922f1655aeb5e6588dc7ed3b0b296d434c0
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2020-09-29 11:09:35 +0200

    fix warning, fix ci

--- rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotIcons.kt
+++ rider-godot/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotIcons.kt
@@ -5,7 +5,7 @@ import com.intellij.openapi.util.IconLoader
 class GodotIcons {
     class Icons {
         companion object {
-            val GodotLogo = IconLoader.getIcon("/Icons/Godot.svg")
+            val GodotLogo = IconLoader.getIcon("/Icons/Godot.svg", GodotIcons::class.java)
         }
     }
 
