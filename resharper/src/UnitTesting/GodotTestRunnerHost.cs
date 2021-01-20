827d27a0475074bd553c323161fe3ce60b20fdba
Ivan Shakhov <Ivan.Shakhov@jetbrains.com>
2021-01-20 13:59:26 +0100

    add support for debug tests

--- resharper/src/UnitTesting/GodotTestRunnerHost.cs
+++ resharper/src/UnitTesting/GodotTestRunnerHost.cs
@@ -2,12 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
+using System.Threading.Tasks;
 using JetBrains.Annotations;
 using JetBrains.Application.Processes;
+using JetBrains.Application.Threading;
 using JetBrains.Collections.Viewable;
+using JetBrains.Core;
+using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Host.Features;
+using JetBrains.ReSharper.UnitTestFramework;
 using JetBrains.ReSharper.UnitTestFramework.Extensions;
+using JetBrains.ReSharper.UnitTestFramework.Launch;
 using JetBrains.ReSharper.UnitTestFramework.Processes;
 using JetBrains.ReSharper.UnitTestFramework.TestRunner;
 using JetBrains.Rider.Model.Godot.FrontendBackend;
@@ -18,11 +24,19 @@ namespace JetBrains.ReSharper.Plugins.Godot.UnitTesting
     public class GodotTestRunnerHost : DefaultTestRunnerHost
     {
         [NotNull] public new static readonly ITestRunnerHost Instance = new GodotTestRunnerHost();
+        private int myDebugPort;
 
         public override IPreparedProcess StartProcess(ProcessStartInfo startInfo, ITestRunnerContext context)
         {
             context.Settings.TestRunner.NoIsolationNetFramework.SetValue(true);
-            
+
+            if (context is ITestRunnerExecutionContext executionContext &&
+                executionContext.Run.HostController.HostId == WellKnownHostProvidersIds.DebugProviderId)
+            {
+                PrepareForRunCore(executionContext.Run).Wait();
+                startInfo.EnvironmentVariables.Add("GODOT_MONO_DEBUGGER_AGENT", $"--debugger-agent=transport=dt_socket,address=127.0.0.1:{myDebugPort},server=n,suspend=y");
+            }
+
             var rawStartInfo = new JetProcessStartInfo(startInfo);
             var patcher = new GodotPatcher(context.RuntimeEnvironment.Project.GetSolution());
             var request = context.RuntimeEnvironment.ToJetProcessRuntimeRequest();
@@ -31,6 +45,42 @@ namespace JetBrains.ReSharper.Plugins.Godot.UnitTesting
         }
 
         public override IEnumerable<Assembly> InProcessAssemblies => EmptyArray<Assembly>.Instance;
+
+
+        private Task PrepareForRunCore(IUnitTestRun run)
+        {
+            var tcs = new TaskCompletionSource<bool>();
+            var taskLifetimeDef = Lifetime.Define(run.Lifetime);
+            taskLifetimeDef.SynchronizeWith(tcs);
+            var taskLifetime = taskLifetimeDef.Lifetime;
+
+            var solution = run.Launch.Solution;
+            var model = solution.GetProtocolSolution().GetFrontendBackendGodotModel();
+            solution.Locks.ExecuteOrQueueEx(taskLifetime, "AttachDebuggerToUnityEditor", () =>
+            {
+                if (!taskLifetime.IsAlive || model == null)
+                {
+                    tcs.TrySetCanceled();
+                    return;
+                }
+
+                var task = model.StartDebuggerServer.Start(taskLifetime, Unit.Instance);
+                task.Result.Advise(taskLifetime, result =>
+                {
+                    if (!run.Lifetime.IsAlive)
+                        tcs.TrySetCanceled();
+                    else if (result.Result <= 0)
+                        tcs.SetException(new Exception("Unable to start debugger."));
+                    else
+                    {
+                        myDebugPort = result.Result;
+                        tcs.SetResult(true);
+                    }
+                });
+            });
+
+            return tcs.Task;
+        }
     }
 
     public class GodotPatcher : IProcessStartInfoPatcher
