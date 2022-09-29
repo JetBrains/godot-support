1b6c4dc6b85b5d9c69e3dc9532c6cdb525be1c1f
David Horacek <horacek.d@hanaboso.com>
2022-09-29 13:38:19 +0200

    Disable deprecated class loader
    GitOrigin-RevId: 56c0bc096b4e6c428dacdae54f40b10c10fb64b2

--- gdscript/src/main/java/gdscript/competion/staticLoader/StaticClassLoader.java
+++ gdscript/src/main/java/gdscript/competion/staticLoader/StaticClassLoader.java
@@ -1,42 +1,35 @@
 package gdscript.competion.staticLoader;
 
-import gdscript.competion.staticLoader.model.GdClass;
-
-import java.io.*;
-import java.util.*;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
-
 @Deprecated
 public class StaticClassLoader {
 
-    private static final HashMap<String, GdClass> classes = new HashMap<>();
-
-    static {
-        try {
-            String folder = "classes_test";
-            GdClassParser parser = new GdClassParser();
-            ClassLoader loader = StaticClassLoader.class.getClassLoader();
-            var directory = loader.getResource(folder).getPath();
-            String src = directory.substring(6, directory.length() - (folder.length() + 2));
-
-            ZipInputStream zip = new ZipInputStream(new FileInputStream(src));
-            while (true) {
-                ZipEntry e = zip.getNextEntry();
-                if (e == null) break;
-                String name = e.getName();
-                if (name.startsWith(folder) && name.endsWith(".xml")) {
-                    GdClass gdClass = parser.parseXmlClass(loader.getResourceAsStream(name));
-                    classes.put(gdClass.name, gdClass);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static HashMap<String, GdClass> getClasses() {
-        return classes;
-    }
+//    private static final HashMap<String, GdClass> classes = new HashMap<>();
+//
+//    static {
+//        try {
+//            String folder = "classes_test";
+//            GdClassParser parser = new GdClassParser();
+//            ClassLoader loader = StaticClassLoader.class.getClassLoader();
+//            var directory = loader.getResource(folder).getPath();
+//            String src = directory.substring(6, directory.length() - (folder.length() + 2));
+//
+//            ZipInputStream zip = new ZipInputStream(new FileInputStream(src));
+//            while (true) {
+//                ZipEntry e = zip.getNextEntry();
+//                if (e == null) break;
+//                String name = e.getName();
+//                if (name.startsWith(folder) && name.endsWith(".xml")) {
+//                    GdClass gdClass = parser.parseXmlClass(loader.getResourceAsStream(name));
+//                    classes.put(gdClass.name, gdClass);
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static HashMap<String, GdClass> getClasses() {
+//        return classes;
+//    }
 
 }
