dc7683d501041eb49126a166d390e92fa6d929f6
Dejv <h.raven@centrum.cz>
2022-12-18 19:38:50 +0100

    ContextId
    GitOrigin-RevId: 26f6bd8e3dfb6e1e154fa519ab58557e1abed625

--- gdscript/src/main/kotlin/gdscript/GdTemplateContextType.kt
+++ gdscript/src/main/kotlin/gdscript/GdTemplateContextType.kt
@@ -3,7 +3,7 @@ package gdscript
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
-class GdTemplateContextType : TemplateContextType("GDSCRIPT", "GdScript") {
+class GdTemplateContextType : TemplateContextType("GdScript") {
 
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean =
         templateActionContext.file.name.endsWith(".gd");
