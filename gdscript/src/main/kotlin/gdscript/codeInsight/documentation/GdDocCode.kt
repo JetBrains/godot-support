fbf141acbfe96e554198a9112a82ee05b4ef0503
David <horacek.d@hanaboso.com>
2023-05-03 16:01:55 +0200

    WIP - GdDocCode
    GitOrigin-RevId: 7bee79fbf528168122d4b85609639aee113494a7

--- gdscript/src/main/kotlin/gdscript/codeInsight/documentation/GdDocCode.kt
+++ gdscript/src/main/kotlin/gdscript/codeInsight/documentation/GdDocCode.kt
@@ -1,15 +1,34 @@
 package gdscript.codeInsight.documentation
 
 import com.intellij.ide.highlighter.HighlighterFactory
+import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.editor.colors.EditorColorsScheme
 import com.intellij.openapi.editor.highlighter.EditorHighlighter
 import com.intellij.openapi.editor.richcopy.HtmlSyntaxInfoUtil
 import com.intellij.openapi.editor.richcopy.SyntaxInfoBuilder
+import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiFileFactory
+import gdscript.GdLanguage
+import gdscript.highlighter.GdSyntaxHighlighter
+import gdscript.highlighter.GdSyntaxHighlighterFactory
 import java.io.IOException
 
 
 object GdDocCode {
+
+    fun createHighlightedSnippet(code: String, project: Project) {
+        val scheme = EditorColorsManager.getInstance().globalScheme
+        val parsedCode = code.replace("\t", "  ")
+        val psi = PsiFileFactory.getInstance(project).createFileFromText(GdLanguage, parsedCode)
+
+        val highlighter = HighlighterFactory.createHighlighter(psi.virtualFile, scheme, project)
+        highlighter.setText(parsedCode)
+
+        val iterator = highlighter.createIterator(0)
+
+        //GdSyntaxHighlighter().getTokenHighlights()
+    }
 
 //    fun getHtmlContent(
 //        file: PsiFile,
