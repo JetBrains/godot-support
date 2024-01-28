d104deb68142ca71f952c48d824ebcf41ecf22ca
Ivan Shakhov <van800@gmail.com>
2024-01-28 17:15:45 +0100

    fix RIDER-102751 Rider crashes in nunit test even when stopping at fi… (#129)
    * fix RIDER-102751 Rider crashes in nunit test even when stopping at first line of test
    
    * cache

--- resharper/debugger/debugger/Evaluation/MonoGodotAdditionalValuesProvider.cs
+++ resharper/debugger/debugger/Evaluation/MonoGodotAdditionalValuesProvider.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using JetBrains.Annotations;
 using JetBrains.ReSharper.Plugins.Godot.Rider.Debugger.Values;
@@ -45,6 +47,7 @@ namespace JetBrains.ReSharper.Plugins.Godot.Rider.Debugger.Evaluation
         private readonly IValueServicesFacade<TValue> myValueServices;
         private readonly IOptions myOptions;
         private readonly ILogger myLogger;
+        private bool? myIsGodotModule;
 
         protected GodotAdditionalValuesProvider(IDebuggerSession session, IValueServicesFacade<TValue> valueServices,
             IOptions options, ILogger logger)
@@ -65,11 +68,31 @@ namespace JetBrains.ReSharper.Plugins.Godot.Rider.Debugger.Evaluation
             // TODO: Make lazy
             if (!myOptions.ExtensionsEnabled || !mySession.EvaluationOptions.AllowTargetInvoke)
                 yield break;
+            
+            // Only needed for the Cor
+            // Avoid evaluating Scene when running not in the Game, for example in the unit-tests
+            if (this is CorGodotAdditionalValuesProvider)
+            {
+                if (!myIsGodotModule.HasValue) 
+                    myIsGodotModule = IsGodotProcess(frame.DebuggerSession);
+                
+                if (!myIsGodotModule.Value) 
+                    yield break;
+            }
 
             // Add "Current Scene" as a top level item to mimic the Hierarchy window in Godot
             var activeScene = GetCurrentScene(frame);
             if (activeScene != null)
                 yield return activeScene.ToValue(myValueServices);
+        }
+
+        private static bool IsGodotProcess(IDebuggerSession session)
+        {
+            var processId = session.GetProcessInfo().Id;
+            if (processId == null) return false;
+            int pid = Convert.ToInt32(processId);
+            var mainModule = Process.GetProcessById(pid).MainModule;
+            return mainModule != null && mainModule.ModuleName.StartsWith("Godot");
         }
 
         [CanBeNull]
