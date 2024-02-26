68d648a8115c84b97b948e821fc13603f858ea4d
Dejv <h.raven@centrum.cz>
2024-02-26 18:04:48 +0100

    revert #220 quickfix params
    GitOrigin-RevId: 891f0bfee6ed19e3f4659c88c510194bbbd97b99

--- gdscript/src/main/kotlin/gdscript/inspection/quickFix/GdAddVariableTypeHintFix.kt
+++ gdscript/src/main/kotlin/gdscript/inspection/quickFix/GdAddVariableTypeHintFix.kt
@@ -7,23 +7,31 @@ import com.intellij.psi.PsiFile
 import com.intellij.psi.util.PsiTreeUtil
 import gdscript.psi.*
 
-class GdAddVariableTypeHintFix(element: PsiElement, private val desired: String, private val fullType: Boolean) : LocalQuickFixOnPsiElement(element) {
+class GdAddVariableTypeHintFix : LocalQuickFixOnPsiElement {
+
+    private val desired: String
+    private val fullType: Boolean
+
+    constructor(element: PsiElement, desired: String, fullType: Boolean) : super(element) {
+        this.desired = desired
+        this.fullType = fullType
+    }
 
     override fun getFamilyName(): String = "Add variable type"
     override fun getText(): String = "Specify variable type [$desired]"
 
     override fun invoke(project: Project, file: PsiFile, startElement: PsiElement, endElement: PsiElement) {
-        if (fullType) fullType(project)
-        else shortType(project)
+        if (fullType) fullType(project, startElement, endElement)
+        else shortType(project, startElement)
     }
 
-    private fun fullType(project: Project) {
+    private fun fullType(project: Project, startElement: PsiElement, endElement: PsiElement) {
         val typeHint = GdElementFactory.typed(project, desired)
 
         endElement.addAfter(typeHint, typedPosition(startElement))
     }
 
-    private fun shortType(project: Project) {
+    private fun shortType(project: Project, startElement: PsiElement) {
         val assigment = PsiTreeUtil.getChildOfType(startElement, GdAssignTyped::class.java) ?: return
 
         assigment.replace(GdElementFactory.shortAssignTyped(project))
