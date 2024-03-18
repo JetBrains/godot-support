1bff01db1b48716b756ade1ec26f90244c1dfb34
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-03-18 18:20:56 +0100

    turn GdCodeStyleSettings to kotlin
    GitOrigin-RevId: 7f41c20c8c5d13965b24bf8b15457338cf65f2d2

---
+++ gdscript/src/main/kotlin/gdscript/formatter/GdCodeStyleSettings.kt
@@ -0,0 +1,29 @@
+package gdscript.formatter
+
+import com.intellij.psi.codeStyle.CodeStyleSettings
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings
+import org.jdom.Element
+
+class GdCodeStyleSettings(settings: CodeStyleSettings) : CustomCodeStyleSettings("GdCodeStyleSettings", settings) {
+
+    /* Black lines */
+    @JvmField var LINES_BEFORE_FUNC_MIN: Int = 2
+    @JvmField var LINES_BEFORE_FUNC_MAX: Int = 2
+    @JvmField var LINES_AFTER_HEADER: Int = 1
+    @JvmField var LINES_IN_BETWEEN_VARIABLE_GROUP: Int = 0
+    @JvmField var LINES_AFTER_VARIABLE_GROUP: Int = 0
+    @JvmField var LINES_BETWEEN_EXPORT_GROUPS: Int = 1
+    @JvmField var LINES_WITHIN_SUITE: Int = 1
+    @JvmField var LINES_AROUND_MULTILINE_VAR: Int = 1
+
+    /* Spacing */
+    @JvmField var SPACE_BEFORE_COMMA: Boolean = false
+    @JvmField var SPACE_AFTER_COMMA: Boolean = true
+    @JvmField var SPACE_BEFORE_COLON: Boolean = false
+    @JvmField var SPACE_AFTER_COLON: Boolean = true
+
+    /* Wrapping and Spaces */
+    @JvmField var ALIGN_COMMENTS: Boolean = true
+    @JvmField var ALIGN_ASSIGNMENTS: Boolean = true
+    @JvmField var ALIGN_AFTER_ASSIGNMENTS: Boolean = true
+}
