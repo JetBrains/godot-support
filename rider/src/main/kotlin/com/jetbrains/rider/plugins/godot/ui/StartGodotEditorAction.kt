ac615d42118f0824ed73efa8b5a15326903798d0
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-04-13 17:34:03 +0200

    fix StartGodotEditorAction with Godot3

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/ui/StartGodotEditorAction.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/ui/StartGodotEditorAction.kt
@@ -8,8 +8,10 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.jetbrains.rider.plugins.godot.run.GodotRunConfigurationGenerator
+import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfigurationType
 import com.jetbrains.rider.run.configurations.dotNetExe.DotNetExeConfiguration
 import com.jetbrains.rider.run.configurations.dotNetExe.DotNetExeConfigurationType
+import com.jetbrains.rider.run.configurations.exe.ExeConfiguration
 import com.jetbrains.rider.run.createEmptyConsoleCommandLine
 import com.jetbrains.rider.run.withRawParameters
 
@@ -26,24 +28,46 @@ open class  StartGodotEditorAction : DumbAwareAction() {
         fun startEditor(project: Project) {
             val runManager = RunManager.getInstance(project)
             val configurationType = ConfigurationTypeUtil.findConfigurationType(DotNetExeConfigurationType::class.java)
-            val runConfiguration =
+            var runConfiguration =
                 runManager.findConfigurationByTypeAndName(configurationType.id, GodotRunConfigurationGenerator.EDITOR_CONFIGURATION_NAME)
 
-            val exeConfiguration = runConfiguration?.configuration as DotNetExeConfiguration
-            val runCommandLine = createEmptyConsoleCommandLine(exeConfiguration.parameters.useExternalConsole)
-                .withEnvironment(exeConfiguration.parameters.envs)
-                .withParentEnvironmentType(if (exeConfiguration.parameters.isPassParentEnvs) {
-                    GeneralCommandLine.ParentEnvironmentType.CONSOLE
-                } else {
-                    GeneralCommandLine.ParentEnvironmentType.NONE
-                })
-                .withExePath(exeConfiguration.parameters.exePath)
-                .withWorkDirectory(exeConfiguration.parameters.workingDirectory)
-                .withRawParameters(exeConfiguration.parameters.programParameters)
+            if (runConfiguration != null){
+                val exeConfiguration =  runConfiguration?.configuration as DotNetExeConfiguration
+                val runCommandLine = createEmptyConsoleCommandLine(exeConfiguration.parameters.useExternalConsole)
+                    .withEnvironment(exeConfiguration.parameters.envs)
+                    .withParentEnvironmentType(if (exeConfiguration.parameters.isPassParentEnvs) {
+                        GeneralCommandLine.ParentEnvironmentType.CONSOLE
+                    } else {
+                        GeneralCommandLine.ParentEnvironmentType.NONE
+                    })
+                    .withExePath(exeConfiguration.parameters.exePath)
+                    .withWorkDirectory(exeConfiguration.parameters.workingDirectory)
+                    .withRawParameters(exeConfiguration.parameters.programParameters)
 
-            logger.info("Starting $runCommandLine")
+                logger.info("Starting $runCommandLine")
 
-            runCommandLine.createProcess()
+                runCommandLine.createProcess()
+            }
+            else {
+                val configurationType2 = ConfigurationTypeUtil.findConfigurationType(GodotDebugRunConfigurationType::class.java)
+                val runConfiguration2 =
+                    runManager.findConfigurationByTypeAndName(configurationType2.id, GodotRunConfigurationGenerator.EDITOR_CONFIGURATION_NAME)
+                val exeConfiguration2 =  runConfiguration2?.configuration as ExeConfiguration
+                val runCommandLine2 = createEmptyConsoleCommandLine(exeConfiguration2.parameters.useExternalConsole)
+                    .withEnvironment(exeConfiguration2.parameters.envs)
+                    .withParentEnvironmentType(if (exeConfiguration2.parameters.isPassParentEnvs) {
+                        GeneralCommandLine.ParentEnvironmentType.CONSOLE
+                    } else {
+                        GeneralCommandLine.ParentEnvironmentType.NONE
+                    })
+                    .withExePath(exeConfiguration2.parameters.exePath)
+                    .withWorkDirectory(exeConfiguration2.parameters.workingDirectory)
+                    .withRawParameters(exeConfiguration2.parameters.programParameters)
+
+                logger.info("Starting $runCommandLine2")
+
+                runCommandLine2.createProcess()
+            }
         }
     }
 }
