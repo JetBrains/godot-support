f9b9edd930b71b848197c4cd9206bfe5aef57a30
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-04-16 11:10:35 +0200

    fix path in GodotTextMateBundleProvider
    GitOrigin-RevId: faec19e63f1830a224f7087bc65658eca33621c8

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/textMate/GodotTextMateBundleProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/textMate/GodotTextMateBundleProvider.kt
@@ -1,12 +1,13 @@
 package com.jetbrains.rider.plugins.godot.textMate
 
 import com.intellij.openapi.application.PathManager
+import com.jetbrains.rider.RiderEnvironment
 import org.jetbrains.plugins.textmate.api.TextMateBundleProvider
 import java.io.FileFilter
 
 class GodotTextMateBundleProvider : TextMateBundleProvider {
     override fun getBundles(): List<TextMateBundleProvider.PluginBundle> {
-        val directories = PathManager.getPluginsDir().resolve("rider-godot").resolve("bundles").toFile()
+        val directories = RiderEnvironment.getBundledBinDir().resolve("bundles")
             .listFiles(FileFilter { it.isDirectory }) ?: return emptyList()
 
         val bundles = directories.map{it.resolve("extension")}.filter { it.isDirectory }
