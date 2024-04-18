b8030ec7fba721d53e1b37b271841c8c29985075
Mikhail Filippov <mikhail.filippov@jetbrains.com>
2024-04-18 11:38:33 +0400

    add initial godot support
    GitOrigin-RevId: 7560139be7bf41b2df078c251dafab61862170ac

--- rider/settings.gradle.kts
+++ rider/settings.gradle.kts
@@ -2,18 +2,27 @@ rootProject.name = "rider-godot"
 
 pluginManagement {
     val rdVersion: String by settings
+    val rdKotlinVersion: String by settings
     repositories {
-        maven { setUrl("https://cache-redirector.jetbrains.com/plugins.gradle.org") }
-        // This is for snapshot version of 'org.jetbrains.intellij.platform' plugin
-        maven { setUrl("https://oss.sonatype.org/content/repositories/snapshots/") }
+        maven("https://cache-redirector.jetbrains.com/intellij-dependencies")
+        maven("https://cache-redirector.jetbrains.com/plugins.gradle.org")
+        maven("https://cache-redirector.jetbrains.com/maven-central")
+
+        if (rdVersion == "SNAPSHOT") {
+            mavenLocal()
+        }
     }
+
     plugins {
-        id ("com.jetbrains.rdgen") version rdVersion
+        id("com.jetbrains.rdgen") version rdVersion
+        id("org.jetbrains.kotlin.jvm") version rdKotlinVersion
     }
+
     resolutionStrategy {
         eachPlugin {
-            when(requested.id.name) {
-                // This required to correctly rd-gen plugin resolution. May be we should switch our naming to match Gradle plugin naming convention.
+            when (requested.id.name) {
+                // This required to correctly rd-gen plugin resolution.
+                // Maybe we should switch our naming to match Gradle plugin naming convention.
                 "rdgen" -> {
                     useModule("com.jetbrains.rd:rd-gen:${rdVersion}")
                 }
