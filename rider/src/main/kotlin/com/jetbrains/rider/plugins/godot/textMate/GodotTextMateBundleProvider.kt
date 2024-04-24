5366359b1747aa233d0a6a28decb9e8a74deefa6
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-04-24 16:53:48 +0200

    fix GodotTextMateBundleProvider
    GitOrigin-RevId: 010478d951e02d98b531abfc20c83ae46d0bceb9

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/textMate/GodotTextMateBundleProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/textMate/GodotTextMateBundleProvider.kt
@@ -3,18 +3,24 @@ package com.jetbrains.rider.plugins.godot.textMate
 import com.intellij.openapi.application.PathManager
 import com.jetbrains.rider.RiderEnvironment
 import org.jetbrains.plugins.textmate.api.TextMateBundleProvider
+import java.io.File
 import java.io.FileFilter
 
 class GodotTextMateBundleProvider : TextMateBundleProvider {
     override fun getBundles(): List<TextMateBundleProvider.PluginBundle> {
-        // for regular run
-        var directories = PathManager.getPluginsDir().resolve("rider-godot").resolve("dotnet").resolve("bundles")
-                              .toFile().listFiles(FileFilter { it.isDirectory })
+        // regular run (bundled or non-bundled plugin)
+        val pluginDirs = arrayListOf(
+            File(PathManager.getPluginsPath()),
+            File(PathManager.getPreInstalledPluginsPath()))
+
+        var directories = pluginDirs.flatMap { dir ->
+            dir.resolve("rider-godot").resolve("dotnet").resolve("bundles")
+            .listFiles(FileFilter { it.isDirectory }).orEmpty().toList()}.filterNotNull()
+
         // fallback for run in the ultimate mono-repo
-        if (directories == null || !directories.any()) {
+        if (!directories.any()) {
             directories = RiderEnvironment.getBundledBinDir().resolve("bundles").listFiles(FileFilter { it.isDirectory })
-                          ?: return emptyList()
-        }
+                .orEmpty().toList()}
 
         val bundles = directories.map{it.resolve("extension")}.filter { it.isDirectory }
             .map { TextMateBundleProvider.PluginBundle(it.name, it.toPath()) }
