b9d03f07ebbe09800b3e8c36e0b176f3ef38d8ab
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-04-26 11:02:17 +0200

    split Godot ChatContextPartProviders
    GitOrigin-RevId: 8d5e494830fa7720393016668b9e1b693b04a01f

--- resharper/src/AI/GodotChatContextPartProvider.cs
+++ resharper/src/AI/GodotChatContextPartProvider.cs
@@ -11,6 +11,53 @@ using JetBrains.ReSharper.Plugins.Godot.ProjectModel;
 namespace JetBrains.ReSharper.Plugins.Godot.AI;
 
 [ShellComponent(Instantiation.DemandAnyThread)]
+public class GodotPreferableLanguageChatContextPartProvider : IChatContextPartProvider
+{
+    public int Priority => ChatContextPartPriorities.SOLUTION + 1;
+
+    public bool IsApplicable(IDataContext dataContext)
+    {
+        return true;
+    }
+
+    public void ContributeTo(IDataContext dataContext, ChatContextPartSet parts)
+    {
+        var solution = dataContext.GetData(ProjectModelDataConstants.SOLUTION);
+        if (solution == null)
+            return;
+
+        var tracker = solution.TryGetComponent<GodotTracker>();
+        if (tracker == null) return;
+
+        if (tracker.GodotDescriptor == null) return;
+
+        var text = new StringBuilder();
+        if (tracker.GodotDescriptor.IsPureGdScriptProject)
+            text.Append("Default language in the current project is GDScript.\n");
+        else
+        {
+            var sdk = tracker.MainProject.ProjectProperties.DotNetCorePlatform?.Sdk;
+            if (sdk != null)
+            {
+                text.Append($"Default language in the current project is C#.\n");
+                text.Append($"Project SDK is \"{sdk}\".\n");
+            }
+        }
+    
+        parts.Add(new Part(text.ToString()));
+    }
+  
+    public sealed class Part : SimpleChatContextPart<Part>, ILanguageDefinitionContextPart
+    {
+        public override int Priority => ChatContextPartPriorities.SOLUTION + 1;
+
+        public Part(string text) : base(text)
+        {
+        }
+    }
+}
+
+[ShellComponent(Instantiation.DemandAnyThread)]
 public class GodotChatContextPartProvider : IChatContextPartProvider
 {
   public int Priority => ChatContextPartPriorities.SOLUTION + 1;
@@ -33,22 +80,9 @@ public class GodotChatContextPartProvider : IChatContextPartProvider
 
     if (parts.Contains<TechnologyChatContextPartProvider.Part>())
       parts.Remove<TechnologyChatContextPartProvider.Part>();
-    
-    var text = new StringBuilder();
-    text.Append("You are working with Godot project.\n");
-    if (tracker.GodotDescriptor.IsPureGdScriptProject)
-        text.Append("Default language in the project is GDScript.\n");
-    else
-    {
-        var sdk = tracker.MainProject.ProjectProperties.DotNetCorePlatform?.Sdk;
-        if (sdk != null)
-        {
-            text.Append($"Default language in the project is C#.\n");
-            text.Append($"Project SDK is \"{sdk}\".\n");
-        }
-    }
-    
-    parts.Add(new Part(text.ToString()));
+
+    var text = "Current project is using following technology: Godot.";
+    parts.Add(new Part(text));
   }
   
   public sealed class Part : SimpleChatContextPart<Part>
