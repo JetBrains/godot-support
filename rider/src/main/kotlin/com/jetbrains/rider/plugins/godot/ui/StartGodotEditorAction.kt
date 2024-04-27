e70fa33157598d49160a139e7989b6f0e19082f3
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-04-27 17:31:54 +0200

    fix StartEditor action for Godot 3
    GitOrigin-RevId: 2346fd5e559a545b29294af0b1ed665b90801453

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/ui/StartGodotEditorAction.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/ui/StartGodotEditorAction.kt
@@ -8,6 +8,8 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.project.Project
 import com.jetbrains.rider.plugins.godot.run.GodotRunConfigurationGenerator
+import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfiguration
+import com.jetbrains.rider.plugins.godot.run.configurations.GodotDebugRunConfigurationType
 import com.jetbrains.rider.run.configurations.dotNetExe.DotNetExeConfiguration
 import com.jetbrains.rider.run.configurations.dotNetExe.DotNetExeConfigurationType
 import com.jetbrains.rider.run.createEmptyConsoleCommandLine
@@ -23,23 +25,57 @@ open class  StartGodotEditorAction : DumbAwareAction() {
 
     companion object {
         private val logger = Logger.getInstance(StartGodotEditorAction::class.java)
+
+        data class Parameters(val useExternalConsole:Boolean, val envs: Map<String, String>, val isPassParentEnvs:Boolean, val exePath: String,
+                              val workingDirectory:String, val programParameters:String)
         fun startEditor(project: Project) {
             val runManager = RunManager.getInstance(project)
             val configurationType = ConfigurationTypeUtil.findConfigurationType(DotNetExeConfigurationType::class.java)
-            val runConfiguration =
-                runManager.findConfigurationByTypeAndName(configurationType.id, GodotRunConfigurationGenerator.EDITOR_CONFIGURATION_NAME)
+            var runConfiguration =
+                runManager.findConfigurationByTypeAndName(
+                    configurationType.id,
+                    GodotRunConfigurationGenerator.EDITOR_CONFIGURATION_NAME
+                )
 
-            val exeConfiguration = runConfiguration?.configuration as DotNetExeConfiguration
-            val runCommandLine = createEmptyConsoleCommandLine(exeConfiguration.parameters.useExternalConsole)
-                .withEnvironment(exeConfiguration.parameters.envs)
-                .withParentEnvironmentType(if (exeConfiguration.parameters.isPassParentEnvs) {
+            if (runConfiguration == null)
+                runConfiguration = runManager.findConfigurationByTypeAndName(
+                    ConfigurationTypeUtil.findConfigurationType(GodotDebugRunConfigurationType::class.java).id,
+                    GodotRunConfigurationGenerator.EDITOR_CONFIGURATION_NAME
+                )
+
+            if (runConfiguration == null)
+                throw Exception("Godot ${GodotRunConfigurationGenerator.EDITOR_CONFIGURATION_NAME} run configuration was not present.")
+
+            val parameters = when (val exeConfiguration = runConfiguration.configuration) {
+                is DotNetExeConfiguration -> Parameters(
+                    exeConfiguration.parameters.useExternalConsole,
+                    exeConfiguration.parameters.envs,
+                    exeConfiguration.parameters.isPassParentEnvs,
+                    exeConfiguration.parameters.exePath,
+                    exeConfiguration.parameters.workingDirectory,
+                    exeConfiguration.parameters.programParameters
+                )
+                is GodotDebugRunConfiguration -> Parameters(
+                    exeConfiguration.parameters.useExternalConsole,
+                    exeConfiguration.parameters.envs,
+                    exeConfiguration.parameters.isPassParentEnvs,
+                    exeConfiguration.parameters.exePath,
+                    exeConfiguration.parameters.workingDirectory,
+                    exeConfiguration.parameters.programParameters
+                )
+                else -> throw Exception("Unexpected run configuration type")
+            }
+
+            val runCommandLine = createEmptyConsoleCommandLine(parameters.useExternalConsole)
+                .withEnvironment(parameters.envs)
+                .withParentEnvironmentType(if (parameters.isPassParentEnvs) {
                     GeneralCommandLine.ParentEnvironmentType.CONSOLE
                 } else {
                     GeneralCommandLine.ParentEnvironmentType.NONE
                 })
-                .withExePath(exeConfiguration.parameters.exePath)
-                .withWorkDirectory(exeConfiguration.parameters.workingDirectory)
-                .withRawParameters(exeConfiguration.parameters.programParameters)
+                .withExePath(parameters.exePath)
+                .withWorkDirectory(parameters.workingDirectory)
+                .withRawParameters(parameters.programParameters)
 
             logger.info("Starting $runCommandLine")
 
