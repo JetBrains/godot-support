c4caf9ce0a61ce91754a9d77481e0536104a4ac6
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-05-03 12:32:32 +0200

    update dotnet used in the ci.yml
    GitOrigin-RevId: 9712406b6cd9e8674d576c13526dc0c1c3fb102f

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/lang/service/GodotLspRunningStatusProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/lang/service/GodotLspRunningStatusProvider.kt
@@ -1,5 +1,6 @@
 package com.jetbrains.rider.plugins.godot.lang.service
 
+import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
 import com.intellij.platform.lsp.api.LspServerManager
 import com.intellij.platform.lsp.api.LspServerState
@@ -9,6 +10,8 @@ class GodotLspRunningStatusProvider : LspRunningStatusProvider {
     override fun isLspRunning(project: Project): Boolean {
         val manager = LspServerManager.getInstance(project)
         val provider = manager.getServersForProvider(GodotLspServerSupportProvider::class.java)
-        return provider.any {it.state == LspServerState.Running}
+        val isRunning = provider.any { it.state == LspServerState.Running }
+        thisLogger().trace("Godot LSP is running ${isRunning}")
+        return isRunning
     }
 }
