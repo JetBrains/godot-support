38c715b27743b8286dfee3ebe7d48f828b95f67f
Mark.Tkachenko <mark.tkachenko@jetbrains.com>
2024-06-11 19:04:08 +0200

    fix RIDER-112527
    GitOrigin-RevId: 856ed06139e64cd4dccc0fe8fb755d0f5222e47d

--- resharper/src/ZoneMarker.cs
+++ resharper/src/ZoneMarker.cs
@@ -1,11 +1,12 @@
 using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services.AI;
 using JetBrains.Rider.Backend.Env;
 using JetBrains.Rider.Backend.Product;
 
 namespace JetBrains.ReSharper.Plugins.Godot
 {
     [ZoneMarker]
-    public class ZoneMarker : IRequire<IRiderProductEnvironmentZone>, IRequire<IRiderFeatureZone>
+    public class ZoneMarker : IRequire<IRiderProductEnvironmentZone>, IRequire<IRiderFeatureZone>, IRequire<IArtificialIntelligenceZone>
     {
     }
 }
