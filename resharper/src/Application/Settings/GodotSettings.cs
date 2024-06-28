7a8c1ff26681917cfaa245d3e1288140c1ce3749
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2024-06-28 13:55:35 +0200

    backend Godot settings
    (cherry picked from commit 55a2484fff67bdb0440fb789025b1562bcb75b11)
    
    NET-MR-14549
    
    (cherry picked from commit a076ad0c4fbfbb48402dcae7915ac0cfa8015e1b)
    
    (cherry picked from commit db86200c4f35a65f20b7d8e8aab35c9ceeca73a2)
    
    NET-MR-14556
    
    GitOrigin-RevId: 2d1008e8b3403ebb1c46198bfb19896bab12857c

--- resharper/src/Application/Settings/GodotSettings.cs
+++ resharper/src/Application/Settings/GodotSettings.cs
@@ -1,29 +1,28 @@
-using System;
-using System.Linq.Expressions;
 using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Plugins.Godot.Resources;
 using JetBrains.ReSharper.Resources.Settings;
 using JetBrains.Rider.Model.Godot.FrontendBackend;
 
 namespace JetBrains.ReSharper.Plugins.Godot.Application.Settings
 {
-    [SettingsKey(typeof(CodeEditingSettings), "Godot plugin settings")]
+    [SettingsKey(typeof(CodeEditingSettings), nameof(Strings.GodotPluginSettings_Text))]
     public class GodotSettings
     {
         // LSP 
-        [SettingsEntry(LanguageServerConnectionMode.ConnectRunningEditor, "Different ways to connect LSP")]
+        [SettingsEntry(LanguageServerConnectionMode.ConnectRunningEditor,  nameof(Strings.LanguageServerConnectionMode_Description))]
         public LanguageServerConnectionMode LanguageServerConnectionMode;
 
-        [SettingsEntry("127.0.0.1", "RemoteHost")]
+        [SettingsEntry("127.0.0.1", nameof(Strings.GDScript_LSP_Remote_Host_Text))]
         public string RemoteHost;
         
-        [SettingsEntry(6005, "Remote host port")]
+        [SettingsEntry(6005, nameof(Strings.GDScript_LSP_Port_Text))]
         public int RemoteHostPort;
 
-        [SettingsEntry(false, "Use a random free port, supported with Godot 4.3+")]
+        [SettingsEntry(false, nameof(Strings.GDScript_UseARandomFreePortSupportedInGodot4_Text))]
         public bool UseDynamicPort;
         
         // Debugging
-        [SettingsEntry(true, "Enable debugger extensions")]
+        [SettingsEntry(true, DescriptionResourceType: typeof(Strings), DescriptionResourceName: nameof(Strings.GodotSettings_t_Enable_debugger_extensions))]
         public bool EnableDebuggerExtensions;
     }
 }
