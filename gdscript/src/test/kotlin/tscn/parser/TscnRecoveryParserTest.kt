ca669ad571fc0972a2e15aa90adc3a0ad3a6f9a7
Dejv <h.raven@centrum.cz>
2024-09-01 12:47:53 +0200

    ci
    GitOrigin-RevId: d4122746c7efd435d071bbc360e497764a4553a6

--- gdscript/src/test/kotlin/tscn/parser/TscnRecoveryParserTest.kt
+++ gdscript/src/test/kotlin/tscn/parser/TscnRecoveryParserTest.kt
@@ -1,28 +1,28 @@
-package tscn.parser
-
-import com.intellij.testFramework.ParsingTestCase
-import tscn.TscnParserDefinition
-
-class TscnRecoveryParserTest : ParsingTestCase {
-
-    constructor(): super("", "tscn", TscnParserDefinition())
-
-    // TODO as noted within bnf - recover while is fired even when rule is not matched nor pinned - required to rollback unless pinned
-    // 2 possible solutions:
-    //  a) custom Parser - preferred
-    //  b) rewrite bnf to single header rule
-    // @Test fun testHeader() = doTest(true)
-
-    override fun getTestDataPath(): String {
-        return "src/test/kotlin/tscn/parser/recoveryData"
-    }
-
-    override fun skipSpaces(): Boolean {
-        return false
-    }
-
-    override fun includeRanges(): Boolean {
-        return true
-    }
-
-}
+//package tscn.parser
+//
+//import com.intellij.testFramework.ParsingTestCase
+//import tscn.TscnParserDefinition
+//
+//class TscnRecoveryParserTest : ParsingTestCase {
+//
+//    constructor(): super("", "tscn", TscnParserDefinition())
+//
+//    // TODO as noted within bnf - recover while is fired even when rule is not matched nor pinned - required to rollback unless pinned
+//    // 2 possible solutions:
+//    //  a) custom Parser - preferred
+//    //  b) rewrite bnf to single header rule
+//    // @Test fun testHeader() = doTest(true)
+//
+//    override fun getTestDataPath(): String {
+//        return "src/test/kotlin/tscn/parser/recoveryData"
+//    }
+//
+//    override fun skipSpaces(): Boolean {
+//        return false
+//    }
+//
+//    override fun includeRanges(): Boolean {
+//        return true
+//    }
+//
+//}
