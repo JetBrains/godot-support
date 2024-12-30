add861497aa935b34f7b8e67a91d310b4ae1ae50
Losos Obecný <h.raven@centrum.cz>
2024-12-30 15:46:34 +0100

    inline
    GitOrigin-RevId: 581613ffd2dcde47091331b6189fdd494cd546e5

--- gdscript/src/main/kotlin/gdscript/psi/GdInlineVariableActionHandler.kt
+++ gdscript/src/main/kotlin/gdscript/psi/GdInlineVariableActionHandler.kt
@@ -2,14 +2,22 @@ package gdscript.psi
 
 import com.intellij.lang.Language
 import com.intellij.lang.refactoring.InlineActionHandler
-import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.runWriteAction
+import com.intellij.openapi.application.runWriteActionAndWait
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.roots.ModuleRootModificationUtil
 import com.intellij.psi.PsiElement
+import com.intellij.psi.search.GlobalSearchScope
+import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.childLeafs
 import com.intellij.psi.util.elementType
 import gdscript.GdLanguage
+import gdscript.utils.PsiReferenceUtil
 
 class GdInlineVariableActionHandler : InlineActionHandler() {
     override fun isEnabledForLanguage(l: Language?): Boolean {
@@ -23,19 +31,31 @@ class GdInlineVariableActionHandler : InlineActionHandler() {
     override fun inlineElement(project: Project?, editor: Editor?, element: PsiElement?) {
         if (editor == null || project == null || element !is GdVarNmi) return
 
-        // TODO error handling
-        val varDecl = element.parent as? GdVarDeclSt ?: return
-        val containingMethod = PsiTreeUtil.getParentOfType(varDecl, GdMethodDeclTl::class.java) ?: return
-        val callExpressions = PsiTreeUtil.findChildrenOfType(containingMethod, GdCallEx::class.java) ?: return
+        val replacementExpr: GdExpr = when (val parent = element.parent) {
+            is GdVarDeclSt -> parent.expr
+            is GdClassVarDeclTl -> parent.expr
+            is GdConstDeclSt -> parent.expr
+            is GdConstDeclTl -> parent.expr
+            else -> null
+        } ?: return
 
-        val usages = callExpressions.flatMap { it.childLeafs }
-            .filter { it.elementType is GdTokenType }
-            .filter { it.text == element.text }
+        val usages = ReferencesSearch.search(element).findAll()
 
-        val replacementExpr = PsiTreeUtil.findChildOfType(varDecl, GdExpr::class.java) ?: return
+//        val containingMethod = PsiTreeUtil.getParentOfType(expr, GdMethodDeclTl::class.java) ?: return
+//        val callExpressions = PsiTreeUtil.findChildrenOfType(containingMethod, GdCallEx::class.java) ?: return
+//
+//        val usages = callExpressions.flatMap { it.childLeafs }
+//            .filter { it.elementType is GdTokenType }
+//            .filter { it.text == element.text }
 
-        TODO("we need to replace the usages with the replacementExpr in a write action")
+//        val replacementExpr = PsiTreeUtil.findChildOfType(varDecl, GdExpr::class.java) ?: return
+
+        // TODO some UI for selection wich references should be replaces?
+        WriteCommandAction.writeCommandAction(project).run<Exception> {
+            usages.forEach {
+                it.element.replace(replacementExpr)
+            }
+        }
     }
-
 
 }
