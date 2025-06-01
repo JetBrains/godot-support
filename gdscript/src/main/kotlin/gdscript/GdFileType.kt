96168ff644c0486ee43b0d52d5b71803eeab7184
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-06-01 16:04:49 +0200

    fix exception NoClassDefFoundError: gdscript/GdFileType
    (cherry picked from commit 9dd8c090d964a7f30ead010f0119d1f2db50c490)
    
    IJ-MR-163162
    
    GitOrigin-RevId: b012cde9d20288f74b3003411d61ff7404d0a08e

--- gdscript/src/main/kotlin/gdscript/GdFileType.kt
+++ gdscript/src/main/kotlin/gdscript/GdFileType.kt
@@ -5,10 +5,7 @@ import javax.swing.Icon
 
 object GdFileType : LanguageFileType(GdLanguage) {
     override fun getName(): String = GdLanguage.id
-
     override fun getDescription(): String = GdScriptBundle.message("language.file_name")
-
     override fun getDefaultExtension(): String = "gd"
-
     override fun getIcon(): Icon = GdScriptPluginIcons.Icons.GDScript
 }
--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/configurations/gdscript/breakpoints/GdScriptDebuggerEditorsProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/configurations/gdscript/breakpoints/GdScriptDebuggerEditorsProvider.kt
@@ -1,18 +1,18 @@
 package com.jetbrains.rider.plugins.godot.run.configurations.gdscript.breakpoints
 
 import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.fileTypes.PlainTextLanguage
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileFactory
 import com.intellij.xdebugger.evaluation.XDebuggerEditorsProviderBase
 import gdscript.GdFileType
+import gdscript.GdLanguage
 
 internal class GdScriptDebuggerEditorsProvider: XDebuggerEditorsProviderBase() {
     override fun getFileType(): FileType = GdFileType
 
     override fun createExpressionCodeFragment(project: Project, text: String, context: PsiElement?, isPhysical: Boolean): PsiFile {
-        return PsiFileFactory.getInstance(project).createFileFromText("expression.gdscript", PlainTextLanguage.INSTANCE, text)
+        return PsiFileFactory.getInstance(project).createFileFromText("expression.gdscript", GdLanguage, text)
     }
 }
