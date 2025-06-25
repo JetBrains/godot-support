3918252faf2ef3c6453ccac687af9004efc47722
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-06-25 10:44:11 +0200

    fix RIDER-127091 Restore gdshader textmate highlighting
    (cherry picked from commit f2a42be0368ed93239447041a533ff267c10eda6)
    
    IJ-CR-166894
    
    GitOrigin-RevId: 5f4bd2b4618f5e015023471b8a8ead2240bfc1ee

---
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/textMate/GodotTextMateBundleProvider.kt
@@ -0,0 +1,33 @@
+package com.jetbrains.rider.plugins.godot.textMate
+
+import com.intellij.openapi.application.PathManager
+import com.jetbrains.rider.RiderEnvironment
+import org.jetbrains.plugins.textmate.api.TextMateBundleProvider
+import java.io.File
+import java.io.FileFilter
+import kotlin.io.path.name
+
+class GodotTextMateBundleProvider : TextMateBundleProvider {
+    override fun getBundles(): List<TextMateBundleProvider.PluginBundle> {
+        // regular run (bundled or non-bundled plugin)
+        val pluginDirs = arrayListOf(
+            File(PathManager.getPluginsPath()),
+            File(PathManager.getPreInstalledPluginsPath()))
+
+        var directories = pluginDirs.flatMap { dir ->
+            dir.resolve("rider-godot").resolve("dotnet").resolve("bundles")
+            .listFiles(FileFilter { it.isDirectory }).orEmpty().toList()}.filterNotNull()
+
+        // fallback for run in the ultimate mono-repo
+        if (!directories.any()) {
+            directories = RiderEnvironment.getBundledBinDir().resolve("bundles").listFiles(FileFilter { it.isDirectory })
+                .orEmpty().toList()}
+
+        val bundles = directories.map{it.resolve("extension")}.filter { it.isDirectory }
+            .map { dir ->
+                val dirPath = dir.toPath()
+                TextMateBundleProvider.PluginBundle(dirPath.parent.name, dirPath) }
+
+        return bundles
+    }
+}
