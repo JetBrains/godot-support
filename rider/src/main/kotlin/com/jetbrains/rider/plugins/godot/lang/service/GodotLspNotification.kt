98c5b7ea1612c4e3561b38ce45cd92ca4121d52c
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-06-27 17:23:54 +0200

    fix RIDER-127129 Avoid LSP connection to different project
    (cherry picked from commit 005667fc22801d3ebcc89f622265672b8c897ee5)
    
    IJ-CR-167320
    
    GitOrigin-RevId: 7f7bb70427e958e638ea91fe43785434d64822e2

---
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/lang/service/GodotLspNotification.kt
@@ -0,0 +1,56 @@
+package com.jetbrains.rider.plugins.godot.lang.service
+
+import com.intellij.notification.*
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.Service
+import com.intellij.openapi.components.service
+import com.intellij.openapi.project.Project
+import com.jetbrains.rd.platform.protocol.EdtScheduler
+import com.jetbrains.rd.util.lifetime.SequentialLifetimes
+import com.jetbrains.rd.util.threading.coroutines.launch
+import com.jetbrains.rider.plugins.godot.GodotPluginBundle
+import com.jetbrains.rider.plugins.godot.GodotProjectLifetimeService
+import com.jetbrains.rider.plugins.godot.actions.StartGodotEditorAction
+
+@Service(Service.Level.PROJECT)
+class GodotLspNotification(val project: Project) {
+
+    companion object {
+        private const val GROUP_ID = "GodotSupportNotificationGroupId"
+        
+        fun getService(project: Project): GodotLspNotification = project.service<GodotLspNotification>()
+    }
+
+    private val pluginLifetime = GodotProjectLifetimeService.getLifetime(project)
+    private val nestedLifetimeDef = pluginLifetime.createNested()
+    private val sequentialLifetimes = SequentialLifetimes(nestedLifetimeDef)
+
+    /**
+     * Shows a warning notification when LSP attempts to connect to a non-matching project
+     */
+    fun showNonMatchingProjectWarning() {
+        val title = GodotPluginBundle.message("notification.title.godot.lsp.warning")
+        val content = GodotPluginBundle.message("notification.content.lsp.attempted.to.connect.to.non.matching.project")
+        val notificationLifetime = sequentialLifetimes.next()
+
+        val notification = Notification(
+            GROUP_ID,
+            title,
+            content,
+            NotificationType.WARNING
+        )
+
+        notification.addAction(object : NotificationAction(
+            GodotPluginBundle.message("action.StartEditorAction.text")) {
+            override fun actionPerformed(e: AnActionEvent, notification: Notification) {
+                StartGodotEditorAction.startEditor(project)
+                notificationLifetime.terminate()
+            }
+        })
+
+        notificationLifetime.launch(EdtScheduler) {
+            Notifications.Bus.notify(notification, project)
+            notificationLifetime.onTermination { notification.expire() }
+        }
+    }
+}
