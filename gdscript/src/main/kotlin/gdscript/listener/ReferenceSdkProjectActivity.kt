4edc03ea9a2d8df94a12b2b9feaed6ad17d907c7
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-07-01 09:04:07 +0200

    still update gdscript sdk, when there is no Godot support plugin
    (cherry picked from commit 2e231c6db628f69b40ee84de5fdc9af93a1c9465)
    
    IJ-CR-167593
    
    GitOrigin-RevId: a14ef11f25eb3f589ebddaa04d4985fa45328cb2

--- gdscript/src/main/kotlin/gdscript/listener/ReferenceSdkProjectActivity.kt
+++ gdscript/src/main/kotlin/gdscript/listener/ReferenceSdkProjectActivity.kt
@@ -1,14 +1,23 @@
 package gdscript.listener
 
+import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import gdscript.library.GdLibraryUpdater
 import gdscript.utils.RiderGodotSupportPluginUtil
+import gdscript.utils.getMainProjectBasePath
+import gdscript.utils.isRiderGodotSupportPluginInstalled
 
 class ReferenceSdkProjectActivity : ProjectActivity {
 
     override suspend fun execute(project: Project) {
         if (project.isDisposed) return
+
+        if (!PluginManagerCore.isRiderGodotSupportPluginInstalled()){
+            val basePath = project.getMainProjectBasePath() ?: return
+            GdLibraryUpdater.getInstance(project).scheduleSkdCheck(basePath)
+        }
+
         val prop = RiderGodotSupportPluginUtil.getMainProjectBasePathProperty(project) ?: return
         val path = prop.await()
         GdLibraryUpdater.getInstance(project).scheduleSkdCheck(path)
