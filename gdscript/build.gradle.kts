537bfd90612af3bb6e65575ef7134d0d9ff03e9d
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-07-03 12:45:17 +0200

    download gdscriptsdk

--- gdscript/build.gradle.kts
+++ gdscript/build.gradle.kts
@@ -1,5 +1,11 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import kotlin.io.path.Path
+import kotlin.io.path.pathString
 
 plugins {
     alias(libs.plugins.changelog)
@@ -57,21 +63,44 @@ intellijPlatform{
     buildSearchableOptions = buildConfiguration != "Debug"
 }
 
-sourceSets {
-    main {
-        kotlin.srcDir("src/main/kotlin")
-        resources.srcDir("src/main/resources")
-    }
-}
-
 tasks {
     register("prepare") {
-        // todo: get sdk from  https://packages.jetbrains.team/files/p/net/gdscriptsdk/
-        // https://youtrack.jetbrains.com/issue/RIDER-127007/Different-approach-to-GD-sdk
+        doLast {
+            val url = "https://packages.jetbrains.team/files/p/net/gdscriptsdk/gdscriptsdk-1.0.0-SNAPSHOT.tar.xz"
+            val sdkDir = project.layout.buildDirectory.dir("sdk").get().asFile
+            
+            // Create the SDK directory if it doesn't exist
+            if (!sdkDir.exists()) {
+                sdkDir.mkdirs()
+            }
+            
+            // Download the SDK
+            val sdkFile = sdkDir.resolve("sdk.tar.xz")
+            if (sdkFile.exists()) {
+                return@doLast
+            }
+            val client = HttpClient.newBuilder()
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .build()
+            val request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build()
+
+            client.send(
+                request,
+                HttpResponse.BodyHandlers.ofFile(sdkFile.toPath())
+            )
+            
+            println("Downloaded SDK from $url to ${sdkFile.absolutePath}")
+        }
     }
 
-    processResources {
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    prepareSandbox{
+        dependsOn("prepare")
+        val pluginName = intellijPlatform.projectName.get()
+        val sdkDir = project.layout.buildDirectory.dir("sdk").get().asFile
+        from(sdkDir) { into(Path(pluginName, "sdk").pathString)}
+
     }
 
     runIde {
