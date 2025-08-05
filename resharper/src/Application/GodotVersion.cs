5ffde4039892774ee26b07e8558a84c1485ec80e
george.lemeshko <heorhii.lemeshko@jetbrains.com>
2025-08-05 12:40:11 +0200

    [rider][junie][godot] Integrate Godot version tracking
    (cherry picked from commit 2208659d88dde20367a21e61d0e22f97ceb53780)
    
    NET-CR-17080
    
    GitOrigin-RevId: f853562dfb97137003d79a81bf0ac6dca9afc08b

---
+++ resharper/src/Application/GodotVersion.cs
@@ -0,0 +1,102 @@
+#nullable enable
+using System;
+using System.Text.RegularExpressions;
+using JetBrains.Application.FileSystemTracker;
+using JetBrains.Application.Parts;
+using JetBrains.Collections.Viewable;
+using JetBrains.Lifetimes;
+using JetBrains.ProjectModel;
+using JetBrains.Rd.Base;
+using JetBrains.ReSharper.Plugins.Godot.ProjectModel;
+using JetBrains.Util;
+using JetBrains.Util.Logging;
+
+namespace JetBrains.ReSharper.Plugins.Godot.Application;
+
+[SolutionComponent(Instantiation.DemandAnyThreadSafe)]
+public class GodotVersion : IGodotVersion
+{
+    private readonly IFileSystemTracker myFileSystemTracker;
+    private Version? myVersionFromProjectVersionTxt;
+    private readonly VirtualFileSystemPath? mySolutionDirectory;
+    private static readonly ILogger ourLogger = Logger.GetLogger<GodotVersion>();
+    public ViewableProperty<Version> ActualVersionForSolution { get; } = new(new Version(0, 0));
+
+    public GodotVersion(ISolution solution, IFileSystemTracker fileSystemTracker, GodotTracker? godotTracker, Lifetime lifetime)
+    {
+        myFileSystemTracker = fileSystemTracker;
+        mySolutionDirectory = solution.SolutionDirectory;
+        if (mySolutionDirectory is { IsAbsolute: false })
+            mySolutionDirectory = solution.SolutionDirectory.ToAbsolutePath( FileSystemUtil.GetCurrentDirectory().ToVirtualFileSystemPath());
+
+        if (godotTracker != null)
+            SetActualVersionForSolution(lifetime); 
+    }
+
+    private static VirtualFileSystemPath GetProjectFilePath(VirtualFileSystemPath solutionDirectory)
+    {
+        var projectVersionTxtPath = solutionDirectory.Combine("project.godot");
+        return projectVersionTxtPath;
+    }
+    private Version? TryGetVersionFromProjectVersion(VirtualFileSystemPath solutionDirectory)
+    {
+        var version = GetProjectSettingsUnityVersion(solutionDirectory);
+        if (version == null)
+            return null;
+            
+        return Parse(version);
+    }
+
+    private static Version? Parse(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+            return null;
+        return Version.TryParse(input, out var version) ? version : null;
+    }
+
+    private static string? GetProjectSettingsUnityVersion(VirtualFileSystemPath solutionDirectory)
+    {
+        var projectVersionTxtPath = GetProjectFilePath(solutionDirectory);
+        if (!projectVersionTxtPath.ExistsFile)
+            return null;
+            
+        var text = projectVersionTxtPath.ReadAllText2().Text;
+        var match = Regex.Match(text, @"^config/features=PackedStringArray\(""(?<version>[^""]+)""\)", RegexOptions.Multiline);
+        var groups = match.Groups;
+        if (match.Success)
+            return groups["version"].Value;
+
+        return null;
+    }
+    private void UpdateActualVersionForSolution()
+    {
+        var version = GetActualVersionForSolution();
+        ourLogger.Verbose($"UpdateActualVersionForSolution to {version}");
+        ActualVersionForSolution.SetValue(version);
+    }
+    private Version GetActualVersionForSolution()
+    {
+        if (myVersionFromProjectVersionTxt != null)
+            return myVersionFromProjectVersionTxt;
+
+        return new Version(0, 0);
+    }
+
+    private void SetActualVersionForSolution(Lifetime lt)
+    {
+        if (mySolutionDirectory == null)
+            return;
+        
+        var projectVersionTxtPath = GetProjectFilePath(mySolutionDirectory);
+        myFileSystemTracker.AdviseFileChanges(lt,
+            projectVersionTxtPath,
+            _ =>
+            {
+                myVersionFromProjectVersionTxt = TryGetVersionFromProjectVersion(mySolutionDirectory);
+                UpdateActualVersionForSolution();
+            });
+        myVersionFromProjectVersionTxt = TryGetVersionFromProjectVersion(mySolutionDirectory);
+
+        UpdateActualVersionForSolution();
+    }
+}
--- resharper/src/UsageStatistics/GodotProjectTechnologyProvider.cs
+++ resharper/src/UsageStatistics/GodotProjectTechnologyProvider.cs
@@ -2,34 +2,40 @@ using System.Collections.Generic;
 using JetBrains.Application.Parts;
 using JetBrains.IDE.UsageStatistics;
 using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Plugins.Godot.Application;
 using JetBrains.ReSharper.Plugins.Godot.ProjectModel;
 
-namespace JetBrains.ReSharper.Plugins.Godot.UsageStatistics
+namespace JetBrains.ReSharper.Plugins.Godot.UsageStatistics;
+
+[SolutionComponent(Instantiation.DemandAnyThreadSafe)]
+public class GodotProjectTechnologyProvider(IGodotVersion godotVersion, GodotTracker godotTracker)
+    : IProjectTechnologyProvider
 {
-    [SolutionComponent(Instantiation.DemandAnyThreadSafe)]
-    public class GodotProjectTechnologyProvider : IProjectTechnologyProvider
+    public IEnumerable<string> GetProjectTechnology(IProject project)
     {
-        public IEnumerable<string> GetProjectTechnology(IProject project)
+        if (project.IsGodotProject())
         {
-            if (project.IsGodotProject())
-            {
-                yield return "Godot";
-                yield return "GameDev";
-            }
+            yield return "Godot";
+            yield return "GameDev";
+            yield return $"Godot version: {godotVersion.ActualVersionForSolution.Maybe.ValueOrDefault?.ToString() ?? "Unknown"}" ;
+                
+            if (godotTracker.GodotDescriptor is { IsPureGdScriptProject: true })
+                yield return "Pure GdScript project";
         }
     }
+}
     
-    [SolutionComponent(Instantiation.DemandAnyThreadSafe)]
-    public class GodotSolutionTechnologyProvider(GodotTracker godotTracker) : ISolutionTechnologyProvider
+[SolutionComponent(Instantiation.DemandAnyThreadSafe)]
+public class GodotSolutionTechnologyProvider(GodotTracker godotTracker, IGodotVersion godotVersion) : ISolutionTechnologyProvider
+{
+    public IEnumerable<string> GetSolutionTechnology(ISolution solution)
     {
-        public IEnumerable<string> GetSolutionTechnology(ISolution solution)
+        if (godotTracker.GodotDescriptor is { IsPureGdScriptProject: true })
         {
-            if (godotTracker.GodotDescriptor is { IsPureGdScriptProject: true })
-            {
-                yield return "Godot";
-                yield return "GameDev";
-                yield return "GDScript";
-            }
+            yield return "Godot";
+            yield return "GameDev";
+            yield return "GDScript";
+            yield return $"Godot version: {godotVersion.ActualVersionForSolution.Maybe.ValueOrDefault?.ToString() ?? "Unknown"}" ;
         }
     }
 }
