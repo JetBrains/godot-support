a1570d2b4b930a96804630f7df4aa517ff7c892a
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-08-13 11:11:18 +0200

    Support Dictionary Generics #337 https://github.com/JetBrains/godot-support/issues/337 in the parser
    (cherry picked from commit f7dd7ae886c5ff154d92d965147ea38859afcaeb)
    
    IJ-CR-172294
    
    GitOrigin-RevId: f3c8282642bde1a2fa810dd418e65cee2b87b57f

---
+++ gdscript/src/test/testData/gdscript/parser/data/TypedDictionary.gd
@@ -0,0 +1,4 @@
+var skills: Dictionary[String, String] = {}
+var scores: Dictionary[String, int] = {"player1": 100}
+var config: Dictionary[String, float] = {"speed": 2.5}
+var typed_value: Dictionary[Variant, int] = { "any value": 0, 123: 456, null: -1 }
