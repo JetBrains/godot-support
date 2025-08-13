e6db0e957fceb76e7b095f3910adb3e2a9e24b40
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-08-13 13:04:01 +0200

    remove rider reference in tests

--- gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/parser/GdComplexParserTest.kt
+++ gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/parser/GdComplexParserTest.kt
@@ -2,9 +2,7 @@ package com.jetbrains.godot.gdscript.parser
 
 import com.intellij.testFramework.ParsingTestCase
 import com.jetbrains.godot.getBaseTestDataPath
-import com.jetbrains.rider.test.framework.TestProcessorRule
 import gdscript.GdParserDefinition
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -12,8 +10,6 @@ import kotlin.io.path.pathString
 
 @RunWith(JUnit4::class)
 class GdComplexParserTest : ParsingTestCase("", "gd", GdParserDefinition()) {
-    @Rule @JvmField val testProcessorRule: TestProcessorRule = TestProcessorRule()
-
     @Test fun testAttributes() = doTest(true)
     @Test fun testSingleLineEnum() = doTest(true)
     @Test fun testBoolWithComparison() = doTest(true)
