56743118e7d75600b0b96850d744b46f9e961922
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-08-23 18:30:19 +0200

    fix RIDER-129198 GdUnusedVariableInspection should not try to search outside gdscript/tscn/tres
    (cherry picked from commit 4823e97940f7820ef0841d9689ec90f99084a224)
    
    IJ-CR-173377
    
    GitOrigin-RevId: dfb9da1c6edf28d1c9a5c4111c06e4ae52b1c962

--- gdscript/src/main/kotlin/gdscript/utils/ProjectUtil.kt
+++ gdscript/src/main/kotlin/gdscript/utils/ProjectUtil.kt
@@ -3,11 +3,16 @@ package gdscript.utils
 import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.search.ProjectScope
+import com.intellij.psi.search.GlobalSearchScope.getScopeRestrictedByFileTypes
+import com.jetbrains.rider.godot.community.gdscript.GdFileType
+import tscn.TresFileType
+import tscn.TscnFileType
 
 object ProjectUtil {
 
     fun Project.contentScope(): GlobalSearchScope {
-        return ProjectScope.getContentScope(this)
+        val base = ProjectScope.getContentScope(this)
+        return getScopeRestrictedByFileTypes(base, GdFileType, TscnFileType, TresFileType)
     }
 
 }
