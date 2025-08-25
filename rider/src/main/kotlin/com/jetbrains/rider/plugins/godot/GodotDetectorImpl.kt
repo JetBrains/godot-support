81db0a1caf608804f069203a0d76d1e158bbd8af
Aleksei Priakhin <aleksei.priakhin@jetbrains.com>
2025-08-25 17:35:29 +0200

    [rider] RIDER-129095 added ready backend checking in context
    (cherry picked from commit 55569a4abeed8dba43dab85f8318285712d33986)
    
    NET-CR-17256
    
    GitOrigin-RevId: 323519b87de7917bb0bda793cb72b3f49586d364

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotDetectorImpl.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotDetectorImpl.kt
@@ -2,19 +2,19 @@ package com.jetbrains.rider.plugins.godot
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.rd.util.lifetime
-import com.jetbrains.rd.util.threading.coroutines.asCoroutineDispatcher
+import com.jetbrains.rd.util.threading.coroutines.nextTrueValue
 import com.jetbrains.rider.godot.GodotDetector
 import com.jetbrains.rider.godot.GodotProjectInfo
 import com.jetbrains.rider.model.aIChatModel
 import com.jetbrains.rider.projectView.solution
-import com.jetbrains.rider.protocol.protocol
-import kotlinx.coroutines.withContext
 
-class GodotDetectorImpl: GodotDetector {
+class GodotDetectorImpl : GodotDetector {
     override suspend fun godotProjectInfo(project: Project): GodotProjectInfo {
-        val result = withContext(project.protocol.scheduler.asCoroutineDispatcher) {
-            project.solution.aIChatModel.godotContext.startSuspending(project.lifetime, Unit)
-        }
+        // Wait for the model to be ready before proceeding
+        project.solution.aIChatModel.isReady.nextTrueValue()
+
+        val result = project.solution.aIChatModel.godotContext.startSuspending(project.lifetime, Unit)
+
         return GodotProjectInfo(result.isGodot, result.isPureGdScriptProject, result.sdk)
     }
 }
