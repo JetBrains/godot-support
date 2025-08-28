6fd3fa4a86e6df187f12474b41db09ff2e86abfc
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-08-28 22:17:59 +0200

    fixes for Godot ai context
    (cherry picked from commit b4e9b373358b5866385c52a151c38cc1228637d7)
    
    IJ-CR-173936
    
    GitOrigin-RevId: 3ad1892c9a896133ec90e82c58552478b5e81ab4

--- resharper/src/ProjectModel/GodotTracker.cs
+++ resharper/src/ProjectModel/GodotTracker.cs
@@ -19,66 +19,54 @@ using JetBrains.Util.Threading.Tasks;
 namespace JetBrains.ReSharper.Plugins.Godot.ProjectModel
 {
     [SolutionComponent(Instantiation.DemandAnyThreadSafe)]
-    public class GodotTracker : ISolutionLoadTasksInitialSynchronizeSolutionListener2
+    public class GodotTracker(
+        ISolution solution,
+        ILogger logger,
+        ISolutionLoadTasksScheduler loadTasksScheduler,
+        IShellLocks locks,
+        Lifetime lifetime)
+        : ISolutionLoadTasksInitialSynchronizeSolutionListener2
     {
-        private readonly ISolution mySolution;
-        private readonly ILogger myLogger;
-        private readonly ISolutionLoadTasksScheduler myLoadTasksScheduler;
-        private readonly IShellLocks myLocks;
-        private readonly Lifetime myLifetime;
-        public VirtualFileSystemPath MainProjectBasePath { get; private set; }
+        public VirtualFileSystemPath? MainProjectBasePath { get; private set; }
+        public VirtualFileSystemPath? ProjectGodotPath { get; private set; }
         
         public IProject? MainProject { get; private set; }
         public GodotDescriptor? GodotDescriptor { get; private set; }
 
-        /// <summary>
-        /// List of config/feature from the project.godot
-        /// </summary>
-        // public string[] Features { get; private set; }
-        public GodotTracker(ISolution solution, ILogger logger, ISolutionLoadTasksScheduler loadTasksScheduler,
-            IShellLocks locks, Lifetime lifetime)
-        {
-            mySolution = solution;
-            myLogger = logger;
-            myLoadTasksScheduler = loadTasksScheduler;
-            myLocks = locks;
-            myLifetime = lifetime;
-        }
-
         IEnumerable<SolutionLoadTasksListenerExecutionStep> ISolutionLoadTasksInitialSynchronizeSolutionListener2.OnSolutionLoadInitialSynchronizeSolution(OuterLifetime loadLifetime)
         {
-            var barrierCookie = myLoadTasksScheduler.SetTasksBarrier(GetType(), SolutionLoadTaskKinds.Done,
+            var barrierCookie = loadTasksScheduler.SetTasksBarrier(GetType(), SolutionLoadTaskKinds.Done,
                 "Waiting for Godot solution load tasks");
-            myLocks.Tasks.StartNew(myLifetime, Scheduling.FreeThreaded, TaskPriority.AboveNormal,  () =>
+            locks.Tasks.StartNew(lifetime, Scheduling.FreeThreaded, TaskPriority.AboveNormal,  () =>
             {
-                var model = mySolution.GetProtocolSolution().GetGodotFrontendBackendModel();
+                var model = solution.GetProtocolSolution().GetGodotFrontendBackendModel();
                 try
                 {
-                    if (!mySolution.SolutionFilePath.IsEmpty)
+                    if (!solution.SolutionFilePath.IsEmpty)
                     {
                         var project = GetMainGodotProject();
                         if (project == null) return;
                         MainProjectBasePath = project.Location;
                         MainProject = project;
-                        myLogger.Verbose($"Godot MainProjectBasePath: {MainProjectBasePath}");
+                        logger.Verbose($"Godot MainProjectBasePath: {MainProjectBasePath}");
                         GodotDescriptor = new GodotDescriptor(false, MainProjectBasePath.FullPath,
                             MainProject.TargetFrameworkIds.SingleItem().ToRdTargetFrameworkInfo());
 
                     }
                     else
                     {
-                        var files = mySolution.SolutionDirectory.GetChildFiles(3, "project.godot",
+                        var files = solution.SolutionDirectory.GetChildFiles(3, "project.godot",
                             PathSearchFlags.RecurseIntoSubdirectories);
                         var bestMatch = files.OrderBy(it => it.Components.Count()).FirstOrDefault();
                         if (bestMatch == null) return;
                         GodotDescriptor = new GodotDescriptor(true, bestMatch.Directory.FullPath, null);
+                        ProjectGodotPath = bestMatch;
                     }
-                    // todo: Features = ReadFeatures(solution.SolutionDirectory.Combine("project.godot")); 
                 }
                 finally
                 {
                     barrierCookie.Dispose();
-                    myLocks.ExecuteOrQueue("Sync Godot model", () =>
+                    locks.ExecuteOrQueue("Sync Godot model", () =>
                     {
                         if (GodotDescriptor != null) 
                             model.GodotDescriptor.SetValue(GodotDescriptor);
@@ -93,52 +81,17 @@ namespace JetBrains.ReSharper.Plugins.Godot.ProjectModel
         {
             using (ReadLockCookie.Create())
             {
-                foreach (var project in mySolution.GetAllProjects().Where(project => project.IsGodotProject()))
+                foreach (var project in solution.GetAllProjects())
                 {
                     var file = project.Location.Combine("project.godot");
-                    if (file.ExistsFile) return project;
+                    if (file.ExistsFile)
+                    {
+                        ProjectGodotPath = file;
+                        return project;
+                    }
                 }
             }
             return null;
         }
-
-        // RIDER-111425 Design a cache for "project.godot" data
-        // maybe we need an external files module on backend, or track it on the frontend
-        
-        // private string[] ReadFeatures(VirtualFileSystemPath configPath)
-        // {
-        //     try
-        //     {
-        //         return configPath.ReadTextStream(reader =>
-        //         {
-        //             while (reader.ReadLine() is { } line)
-        //             {
-        //                 if (!line.StartsWith("config/features=PackedStringArray")) continue;
-        //                 // define a regex to capture contents inside double quotes
-        //                 var regex = new Regex("\"([^\"]*)\"");
-        //
-        //                 // find all the matches and extract the contents inside quotes
-        //                 var matches = regex.Matches(line);
-        //                 var features = new string[matches.Count];
-        //
-        //                 for (int i = 0; i < matches.Count; i++)
-        //                 {
-        //                     features[i] =
-        //                         matches[i].Groups[1].Value; // the first capturing group is the content inside quotes
-        //                 }
-        //
-        //                 return features;
-        //             }
-        //
-        //             return null;
-        //         });
-        //     }
-        //     catch (Exception ex)
-        //     {
-        //         myLogger.Error(ex);
-        //     }
-        //
-        //     return null;
-        // }
     }
 }
