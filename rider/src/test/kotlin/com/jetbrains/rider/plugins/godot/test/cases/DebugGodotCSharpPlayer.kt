aca6c93d3af6b1f1524ae5b12268cf2320814acb
Ekaterina.Kuznetsova <ekaterina.kuznetsova@jetbrains.com>
2025-09-05 13:48:43 +0300

    Add dotnetSdk to Godot processes in tests
    GitOrigin-RevId: 193f4868a526523b0a23be678833c0dcfa6b4300

--- rider/src/test/kotlin/com/jetbrains/rider/plugins/godot/test/cases/DebugGodotCSharpPlayer.kt
+++ rider/src/test/kotlin/com/jetbrains/rider/plugins/godot/test/cases/DebugGodotCSharpPlayer.kt
@@ -1,5 +1,7 @@
 package com.jetbrains.rider.plugins.godot.test.cases
 
+import com.jetbrains.rd.platform.diagnostics.LogTraceScenario
+import com.jetbrains.rider.diagnostics.LogTraceScenarios
 import com.jetbrains.rider.test.annotations.report.ChecklistItems
 import com.jetbrains.rider.test.annotations.report.Feature
 import com.jetbrains.rider.test.annotations.Solution
@@ -18,19 +20,21 @@ import com.jetbrains.rider.test.scriptingApi.stepInto
 import com.jetbrains.rider.test.scriptingApi.stepOver
 import com.jetbrains.rider.test.scriptingApi.toggleBreakpoint
 import com.jetbrains.rider.test.scriptingApi.waitForPause
-import org.testng.annotations.AfterMethod
+import com.jetbrains.rider.test.tooling.testTools
 import org.testng.annotations.BeforeMethod
 import org.testng.annotations.Test
 
 @Subsystem(SubsystemConstants.GODOT)
 @Feature("Debug C# godot player")
 @Solution("mixed-language-project")
-@TestSettings(sdkVersion = SdkVersion.LATEST_STABLE, buildTool = BuildTool.SDK)
+@TestSettings(sdkVersion = SdkVersion.DOT_NET_9, buildTool = BuildTool.SDK)
 class DebugGodotCSharpPlayer : PerTestSolutionTestBase() {
     private var godotProcess: Process? = null
 
     override val solutionApiFacade: SolutionApiFacade by lazy { RiderExistingSolutionApiFacade() }
 
+    override val traceScenarios: Set<LogTraceScenario>
+        get() = super.traceScenarios + LogTraceScenarios.Debugger
 
     @BeforeMethod(dependsOnMethods = ["startGodot"])
     override fun setUpTestCaseSolution() {
@@ -43,12 +47,9 @@ class DebugGodotCSharpPlayer : PerTestSolutionTestBase() {
             projectName = testMethod.solution!!.name,
             testWorkDirectory = testWorkDirectory,
             solutionSourceRootDirectory = solutionSourceRootDirectory,
-            logPath = testMethod.logDirectory
+            logPath = testMethod.logDirectory,
+            dotnetSdk = testTools().build.dotNetSdk[testMethod.settings.sdk].root.absolutePath,
         )
-    }
-    @AfterMethod(alwaysRun = true)
-    fun killGodot(){
-        stopGodotProcess(godotProcess)
     }
 
     @Test (description = "Debug C# godot player")
@@ -57,7 +58,7 @@ class DebugGodotCSharpPlayer : PerTestSolutionTestBase() {
         attachDebuggerToGodotEditor(project,
             {
                 buildApiFacade.buildWholeSolution()
-                toggleBreakpoint("PlayerLogger.cs", 14)
+                toggleBreakpoint("PlayerLogger.cs", 14) //  GD.Print($"Binary notation: {binaryNotation}");
             }, {
                 waitForPause()
                 dumpFullCurrentData()
@@ -72,7 +73,7 @@ class DebugGodotCSharpPlayer : PerTestSolutionTestBase() {
         attachDebuggerToGodotEditor(project,
             {
                 buildApiFacade.buildWholeSolution()
-                toggleBreakpoint("PlayerLogger.cs", 15)
+                toggleBreakpoint("PlayerLogger.cs", 15)  //_lastLoggedPosition = GlobalPosition;
             },
             {
                 waitForPause()
@@ -90,7 +91,7 @@ class DebugGodotCSharpPlayer : PerTestSolutionTestBase() {
         attachDebuggerToGodotEditor(project,
             {
                 buildApiFacade.buildWholeSolution()
-                toggleBreakpoint("PlayerLogger.cs", 14)
+                toggleBreakpoint("PlayerLogger.cs", 14) //  GD.Print($"Binary notation: {binaryNotation}");
             },
             {
                 val toEvaluate = "binaryNotation / 25"
