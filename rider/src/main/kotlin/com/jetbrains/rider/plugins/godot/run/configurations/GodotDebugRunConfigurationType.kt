6b1128bf46c3b77c35761f8cb2dd09350679b43c
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-09-10 19:43:54 +0200

    debug gdscript config isEditableInDumbMode
    (cherry picked from commit 1d7750ae59c9ecd04faa41844463306f6bdb5504)
    
    IJ-CR-173638
    
    GitOrigin-RevId: b3e0abb82f7eb7ec11a08e7d1ab2a21a0ff8a559

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/configurations/GodotDebugRunConfigurationType.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/run/configurations/GodotDebugRunConfigurationType.kt
@@ -2,14 +2,14 @@ package com.jetbrains.rider.plugins.godot.run.configurations
 
 import GodotDebugRunFactory
 import com.intellij.execution.configurations.ConfigurationTypeBase
+import com.intellij.openapi.project.DumbAware
 import com.jetbrains.rider.plugins.godot.GodotIcons
 import com.jetbrains.rider.plugins.godot.GodotPluginBundle
 
 class GodotDebugRunConfigurationType : ConfigurationTypeBase(id,
                                                              GodotPluginBundle.message("godot.3.start.and.debug"),
                                                              GodotPluginBundle.message("start.godot.and.attach.debugger"),
-                                                             GodotIcons.RunConfigurations.StartAndDebug) {
-
+                                                             GodotIcons.RunConfigurations.StartAndDebug), DumbAware {
     val factory = GodotDebugRunFactory(this)
 
     init {
