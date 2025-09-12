53c41f265d506ca3e5fd910e693c265ef0ec4eae
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-09-12 09:32:52 +0200

    jvmToolchain 21
    GitOrigin-RevId: fac417006b00f05c1989f4ae6b6efbbb757e0745
    
    (cherry picked from commit 82d504237c8a41f05fc4b3af703b0050f2492ce6)
    
    IJ-MR-175405
    
    GitOrigin-RevId: e122fed60e465769ab100c863f16057d99a78b98

--- gdscript/build.gradle.kts
+++ gdscript/build.gradle.kts
@@ -34,6 +34,12 @@ kotlin {
     }
 }
 
+kotlin {
+    jvmToolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 repositories {
     intellijPlatform {
         defaultRepositories()
