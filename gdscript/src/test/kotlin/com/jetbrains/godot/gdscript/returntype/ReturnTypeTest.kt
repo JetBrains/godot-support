767c2b95cd9b01a5c12c408bbca56a7b8455f552
Steven Schoen <steven@stevenschoen.com>
2025-09-12 04:41:58 -0700

    fix RIDER-130081 GDScript analysis never finishes if instantiating doubly nested class
    * Return class ID as GdClassDecl's return type

---
+++ gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/returntype/ReturnTypeTest.kt
@@ -0,0 +1,24 @@
+package com.jetbrains.godot.gdscript.returntype
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.jetbrains.godot.getBaseTestDataPath
+import gdscript.psi.GdClassVarDeclTl
+import org.junit.Ignore
+import kotlin.io.path.pathString
+
+class ReturnTypeTest : BasePlatformTestCase() {
+
+    @Ignore("Checking returnType currently fails in the test's virtual filesystem")
+    fun testReturnTypesOfNestedClassInstantiations() {
+        val file = myFixture.configureByFile(getTestName(false) + ".gd")
+        val varDeclarations = file.children.filterIsInstance<GdClassVarDeclTl>()
+        val var1Declaration = varDeclarations.first { it.name == "var1" }
+        assertEquals("OuterClass.InnerClassLevel1", var1Declaration.returnType)
+        val var2Declaration = varDeclarations.first { it.name == "var2" }
+        assertEquals("OuterClass.InnerClassLevel1.InnerClassLevel2", var2Declaration.returnType)
+    }
+
+    override fun getTestDataPath(): String? {
+        return getBaseTestDataPath().resolve("testData/gdscript/parser/returnType").pathString
+    }
+}
