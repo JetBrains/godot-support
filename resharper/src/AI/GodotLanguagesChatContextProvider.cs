07f9d2c2997fad756e4c6cd68cae8481266fcd3c
Nadezhda.Melnikova <nadezhda.melnikova@jetbrains.com>
2025-09-21 15:58:30 +0200

    [rider][llm] Added ISolutionLanguageDetailsProvider for solution-wide language providers like GodotSolutionLanguageDetailsChatContextProvider
    GitOrigin-RevId: e739e4dfd4906cda6a1f5cccb0ec96ea1c008694

--- resharper/src/AI/GodotLanguagesChatContextProvider.cs
+++ resharper/src/AI/GodotLanguagesChatContextProvider.cs
@@ -1,32 +1,25 @@
+using System.Collections.Generic;
 using JetBrains.Application;
-using JetBrains.Application.DataContext;
 using JetBrains.Application.Parts;
 using JetBrains.ProjectModel;
-using JetBrains.ProjectModel.DataContext;
 using JetBrains.ReSharper.Feature.Services.ChatContexts.Common;
-using JetBrains.ReSharper.Feature.Services.ChatContexts.DataProviders;
 using JetBrains.ReSharper.Plugins.Godot.ProjectModel;
-using JetBrains.ReSharper.Psi.CSharp;
 
 namespace JetBrains.ReSharper.Plugins.Godot.AI;
 
 [ShellComponent(Instantiation.DemandAnyThreadSafe)]
-public class GodotSolutionLanguageDetailsChatContextProvider : ISolutionLanguageDetailsChatContextProvider
+public class GodotSolutionLanguageDetailsChatContextProvider : ISolutionLanguageDetailsProvider
 {
-    public bool IsApplicable(IDataContext dataContext) => true;
-
-    public void ContributeTo(IDataContext dataContext, ChatContextDataSet<LanguageDetails> data)
+    public IEnumerable<LanguageDetails> GetLanguageDetails(ISolution solution)
     {
-        var solution = dataContext.GetData(ProjectModelDataConstants.SOLUTION);
-        var tracker = solution?.TryGetComponent<GodotTracker>();
-        if (tracker?.GodotDescriptor == null) return;
+        var tracker = solution.TryGetComponent<GodotTracker>();
+        if (tracker?.GodotDescriptor == null) return [];
         if (tracker.GodotDescriptor?.IsPureGdScriptProject == true)
-            data.Add(new LanguageDetails("GDScript"));
-        else
-        {
-            var sdk = tracker.MainProject?.ProjectProperties.DotNetCorePlatform?.Sdk;
-            if (sdk != null)
-                data.Add(new LanguageDetails(CSharpLanguage.Name));
-        }
+            return [ new LanguageDetails("GDScript") ];
+        
+        var sdk = tracker.MainProject?.ProjectProperties.DotNetCorePlatform?.Sdk;
+        if (sdk != null)
+            return [ new LanguageDetails("C#") ];
+        return [];
     }
 }
