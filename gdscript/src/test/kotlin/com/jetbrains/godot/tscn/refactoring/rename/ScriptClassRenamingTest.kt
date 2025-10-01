c999a34758d92989fd6ec4f43a84af4af09c03b4
Samuel Nicholas <nicholas.samuel@gmail.com>
2025-10-01 17:53:58 +0930

    Address comments from van800 regarding abstracting a base with root vsf access and using imports.

--- gdscript/src/test/kotlin/com/jetbrains/godot/tscn/refactoring/rename/ScriptClassRenamingTest.kt
+++ gdscript/src/test/kotlin/com/jetbrains/godot/tscn/refactoring/rename/ScriptClassRenamingTest.kt
@@ -1,19 +1,13 @@
 package com.jetbrains.godot.tscn.refactoring.rename
 
-import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.jetbrains.godot.BasePlatformTestCaseWithTestDataVFSAccess
 import com.jetbrains.godot.getBaseTestDataPath
 import kotlin.io.path.pathString
 
-class ScriptClassRenamingTest : BasePlatformTestCase() {
+class ScriptClassRenamingTest : BasePlatformTestCaseWithTestDataVFSAccess() {
+
     override fun getTestDataPath(): String {
         return getBaseTestDataPath().resolve("testData/tscn/refactoring/rename/scriptClassRenaming").pathString
-    }
-
-    override fun setUp() {
-        super.setUp()
-        // Allow reading expected .after files from the test data directory
-        val path = getTestDataPath()
-        com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess.allowRootAccess(testRootDisposable, path)
     }
 
     fun testRename() {
