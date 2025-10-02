ac72c269c01700b879886ff3284d387181dfebb1
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-10-02 10:46:59 +0200

    fix autoloads for non Rider, refactor GdFileResIndexer to produces Godot-style resource paths (e.g., res://foo/bar.tscn) based on VFS

--- gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/resolve/ResolveMultiFileTest.kt
+++ gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/resolve/ResolveMultiFileTest.kt
@@ -1,14 +1,27 @@
 package com.jetbrains.godot.gdscript.resolve
 
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
 class ResolveMultiFileTest : ResolveTestBase() {
+    @Test
     fun testMultiFileReference() {
         val files = loadFilesByTestName()
         val annotated = dumpResolvesForFiles(files)
         assertGold(annotated)
     }
-
+    @Test
     fun testOuterStaticAccess() {
         val files = loadFilesByTestName()
+        val annotated = dumpResolvesForFiles(files)
+        assertGold(annotated)
+    }
+
+    @Test
+    fun testGlobals() {
+        val files = loadFilesFromSubdirAsProjectRoot()
         val annotated = dumpResolvesForFiles(files)
         assertGold(annotated)
     }
