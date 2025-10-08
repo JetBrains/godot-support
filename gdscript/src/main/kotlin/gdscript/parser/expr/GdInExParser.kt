b81d8d822b6f0bf21b99ccebae182119a3d19a84
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-10-08 12:58:40 +0200

    fix RIDER-131062 GDScript: "not in" operator not parsed

--- gdscript/src/main/kotlin/gdscript/parser/expr/GdInExParser.kt
+++ gdscript/src/main/kotlin/gdscript/parser/expr/GdInExParser.kt
@@ -4,8 +4,9 @@ import com.intellij.psi.tree.IElementType
 import gdscript.parser.GdPsiBuilder
 import gdscript.psi.GdTypes.IN
 import gdscript.psi.GdTypes.IN_EX
+import gdscript.psi.GdTypes.NEGATE
 
-// comparison { "in" comparison }
+// comparison { ("in" | "not" "in") comparison }
 object GdInExParser : GdExprBaseParser() {
 
     override val EXPR_TYPE: IElementType = IN_EX
@@ -13,7 +14,17 @@ object GdInExParser : GdExprBaseParser() {
 
     override fun parse(b: GdPsiBuilder, l: Int, optional: Boolean): Boolean {
         if (!b.recursionGuard(l, "InExpr")) return false
-        var ok = b.consumeToken(IN, pin = true)
+
+        // Accept either 'in' or the composite operator 'not in' (lexer: NEGATE followed by IN)
+        var ok: Boolean
+        if (b.followingTokensAre(NEGATE, IN)) {
+            // consume both as operator
+            ok = b.consumeToken(NEGATE, optional = false, pin = true)
+            ok = ok && b.consumeToken(IN)
+        } else {
+            ok = b.consumeToken(IN, pin = true)
+        }
+
         ok = ok && GdExprParser.parseFrom(b, l, optional, POSITION + 1)
         b.errorPin(ok, "expression")
 
