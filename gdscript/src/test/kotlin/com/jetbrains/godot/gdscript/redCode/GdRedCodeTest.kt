910883c9debae98567086c80f5dd2059a7ed7db8
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-11-04 10:08:02 +0100

    [gdscript] fix RIDER-131926 Infinite indexing godot project -Simple Dungeons addon
    (cherry picked from commit 26cd18d745b484882a314127ef50e30fde112c24)
    
    IJ-CR-181260
    
    GitOrigin-RevId: d6df4936395205a3daef4d180185e7d99cbd2df5

--- gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/redCode/GdRedCodeTest.kt
+++ gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/redCode/GdRedCodeTest.kt
@@ -1,9 +1,15 @@
 package com.jetbrains.godot.gdscript.redCode
 
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.junit.Ignore
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
+@RunWith(JUnit4::class)
 class GdRedCodeTest : BasePlatformTestCase() {
     // Red code should NOT appear for this valid enum and typed variable initialization
+    @Test
     fun testEnumHasNoRedCode() {
         val code = """
       |enum _Anim {
@@ -16,21 +22,39 @@ class GdRedCodeTest : BasePlatformTestCase() {
 
         test(code)
     }
-
+    @Test
     fun testDoubleQuoteStringNameLiteral() {
         val code = """var my_var = &"stringname literal""""
         test(code)
     }
-
+    @Test
     fun testSingleQuoteStringNameLiteral() {
         val code = """var my_var = &'stringname literal'"""
         test(code)
     }
-
+    @Test
     fun testPassForClass() {
         val code = """
             |class A:
             |	pass
+        """.trimMargin()
+        test(code)
+    }
+
+    @Test
+    @Ignore("RIDER-131973 gdscript single line lambda is not recognized")
+    fun testSingleLineLambda(){
+        val code = """
+class Mutex1:
+	func lock():
+		pass
+	func unlock():
+		pass
+var t_mutex := Mutex1.new()
+enum BuildStage { NOT_STARTED = -2, PREPARING = -1, PLACE_ROOMS = 0, PLACE_STAIRS = 1, SEPARATE_ROOMS = 2, CONNECT_ROOMS = 3, FINALIZING = 4, DONE = 5 }
+var stage : BuildStage = BuildStage.NOT_STARTED :
+	set(v): t_mutex.lock(); stage = v; t_mutex.unlock();
+	get: t_mutex.lock(); var v = stage; t_mutex.unlock(); return v;
         """.trimMargin()
         test(code)
     }
