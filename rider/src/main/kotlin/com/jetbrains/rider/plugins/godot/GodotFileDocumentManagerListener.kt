fa2029406fde987ef51dec77e5a1c432bb779f7b
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2025-11-26 19:06:53 +0100

    fix RIDER-132470. Build only main Godot game project, only when there is a connection with GodotEditor, drop message to BuildOutput
    (cherry picked from commit 956fbeb0bf488ff5bc09812db23c4653995d1665)
    
    IJ-CR-183762
    
    (cherry picked from commit ba5aedeb0f7478d94fafe5865752db166ae2f96c)
    
    (cherry picked from commit a90b5d7c290356c2455d6aef543343edd231d4b4)
    
    IJ-CR-183989
    
    GitOrigin-RevId: a0412c11bf7ef77ec7352960db24b2b1ec3c07cc

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotFileDocumentManagerListener.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotFileDocumentManagerListener.kt
@@ -2,20 +2,25 @@ package com.jetbrains.rider.plugins.godot
 
 import com.intellij.openapi.components.serviceIfCreated
 import com.intellij.openapi.fileEditor.FileDocumentManagerListener
+import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.util.application
+import com.jetbrains.rd.util.reactive.ViewableMap
 import com.jetbrains.rd.util.reactive.valueOrDefault
 import com.jetbrains.rider.build.BuildHost
 import com.jetbrains.rider.build.BuildParameters
+import com.jetbrains.rider.build.BuildToolWindowService
+import com.jetbrains.rider.model.BuildMessageKind
 import com.jetbrains.rider.model.BuildTarget
-import com.jetbrains.rider.model.rdShellModel
-import com.jetbrains.rider.protocol.protocolHostIfExists
+import com.jetbrains.rider.model.MessageBuildEvent
+import com.jetbrains.rider.model.godot.frontendBackend.GodotEditorState
 import com.jetbrains.rider.model.godot.frontendBackend.godotFrontendBackendModel
+import com.jetbrains.rider.model.rdShellModel
 import com.jetbrains.rider.projectView.solution
+import com.jetbrains.rider.protocol.protocolHostIfExists
 
 class GodotFileDocumentManagerListener : FileDocumentManagerListener {
 
-    // RIDER-127586 Build in Rider when focus moves out so that Inspector in Godot would reload and show exports
     override fun beforeAllDocumentsSaving() {
         val projectManager = serviceIfCreated<ProjectManager>() ?: return
         val openedGodotProjects = projectManager.openProjects.filter { !it.isDisposed
@@ -28,13 +33,28 @@ class GodotFileDocumentManagerListener : FileDocumentManagerListener {
                 if (project.isDisposed) return@invokeLater
                 val buildAutomatically = project.solution.godotFrontendBackendModel.backendSettings.buildAutomatically.valueOrDefault(false)
                 if (!buildAutomatically) return@invokeLater
+                // it makes no sense to build, if Godot is not running
+                if (project.solution.godotFrontendBackendModel.editorState.valueOrDefault(GodotEditorState.Disconnected) != GodotEditorState.Connected)
+                    return@invokeLater
                 val isActive = project.protocolHostIfExists?.protocol?.rdShellModel?.isApplicationActive?.valueOrNull
                 if (isActive != null && !isActive) {
                     val buildHost = BuildHost.getInstance(project)
                     if (buildHost.ready.value && !buildHost.building.value) {
-                        buildHost.requestBuild(BuildParameters(BuildTarget(), emptyList(), silentMode = true)) {}
+                        val mainProjectPath = GodotProjectDiscoverer.getInstance(project).godotDescriptor.valueOrNull?.mainProjectPath ?: return@invokeLater
+                        buildHost.requestBuild(BuildParameters(BuildTarget(), listOf(mainProjectPath), silentMode = true)) {
+                            showAutoBuildNotification(project)
+                        }
                     }
                 }
             }
+    }
+
+    private fun showAutoBuildNotification(project: Project) {
+        val context = BuildToolWindowService.getInstance(project).getOrCreateContext(project)
+        context.showToolWindowIfHidden(false)
+        val message = GodotPluginBundle.message("build.automatically.text")
+        context.addOutputMessage(MessageBuildEvent(message = message,
+                                                   projectId = null,
+                                                   kind = BuildMessageKind.Message), ViewableMap())
     }
 }
