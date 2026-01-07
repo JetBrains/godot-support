5bb634b053f29bea2f69648a095f70371a63fd69
Martin Tomanek <martin.tomanek@jetbrains.com>
2026-01-07 11:22:35 +0100

    RIDER-134322 GDScript - "is not" boolean condition is not parsed correctly
    GitOrigin-RevId: 72e21477439539ed3f5d61206c46b857dd66a6e0

--- gdscript/src/main/kotlin/gdscript/parser/expr/GdIsExParser.kt
+++ gdscript/src/main/kotlin/gdscript/parser/expr/GdIsExParser.kt
@@ -5,6 +5,7 @@ import gdscript.parser.GdPsiBuilder
 import gdscript.parser.common.GdTypedParser
 import gdscript.psi.GdTypes.IS
 import gdscript.psi.GdTypes.IS_EX
+import gdscript.psi.GdTypes.NEGATE
 
 // is = call [ "is" ( IDENTIFIER | BUILTINTYPE ) ] ;
 object GdIsExParser : GdExprBaseParser() {
@@ -14,7 +15,18 @@ object GdIsExParser : GdExprBaseParser() {
 
     override fun parse(b: GdPsiBuilder, l: Int, optional: Boolean): Boolean {
         if (!b.recursionGuard(l, "IsExpr")) return false
-        var ok = b.consumeToken(IS, pin = true)
+
+        // Accept either 'is' or the composite operator 'is not' (lexer: IS followed by NEGATE)
+        var ok: Boolean
+        if (b.followingTokensAre(IS, NEGATE)) {
+            // consume both as operator
+            ok = b.consumeToken(IS, optional = false, pin = true)
+            ok = ok && b.consumeToken(NEGATE)
+        }
+        else {
+            ok = b.consumeToken(IS, pin = true)
+        }
+
         ok = ok && GdTypedParser.typedVal(b, l + 1, false)
         b.errorPin(ok, "type")
 
