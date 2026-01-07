bbc4a853950c15ffaa528529168a2bba6fb87924
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-01-07 14:05:19 +0100

    fix RIDER-134409 .uid and .import file nesting
    (cherry picked from commit 2d71096a5b96d6157e500f3663d70245140a211b)
    
    IJ-CR-187132
    
    GitOrigin-RevId: 6b250b5b9aaa2ae8ccdbd6eb9e75d0437391ec21

--- gdscript/src/main/kotlin/gdscript/structureView/GdFileNesting.kt
+++ gdscript/src/main/kotlin/gdscript/structureView/GdFileNesting.kt
@@ -6,15 +6,16 @@ class GdFileNesting : ProjectViewNestingRulesProvider {
 
     // TODO no idea how to create some wildcards/pattern matches
     override fun addFileNestingRules(consumer: ProjectViewNestingRulesProvider.Consumer) {
+        /* @tool
+        extends EditorScript
+
+        func _run():
+        var extensions := ResourceLoader.get_recognized_extensions_for_type("Resource")
+        print(extensions)
+        */
+
         arrayOf(
-            "svg",
-            "png",
-            "jpg",
-            "jpeg",
-            "webp",
-            "fbx",
-            "glb",
-            "gltf"
+            "tscn", "tres", "anim", "atlastex", "curvetex", "dpitex", "fontdata", "lmbake", "material", "mesh", "meshlib", "meshtex", "mp3str", "multimesh", "occ", "oggvorbisstr", "phymat", "repl", "res", "sample", "scn", "shape", "stylebox", "tex", "theme", "translation", "png", "bmp", "hdr", "jpg", "jpeg", "svg", "tga", "exr", "webp", "ttf", "ttc", "otf", "otc", "woff", "woff2", "pfb", "pfm", "font", "fnt", "csv", "wav", "obj", "glsl", "dae", "escn", "fbx", "gltf", "glb", "blend", "mp3", "ogg"
         ).forEach { consumer.addNestingRule(".$it", ".$it.import") }
 
         // keep in sync with GodotNestingRulesProvider
