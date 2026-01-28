2047bb8ca4145de94e7658c3b45f4172364ee50c
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-01-28 11:55:08 +0100

    fix RIDER-131262 Rider Godot's scene preview doesn't work in c#.
    (cherry picked from commit c62f0c874fb7d1b957b280f8014d1501d838f501)
    
    IJ-CR-189447
    
    GitOrigin-RevId: 2599c8c3fe6be3228422fc92be493cb03a72a066

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotFilesIndexingRuleProvider.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotFilesIndexingRuleProvider.kt
@@ -13,6 +13,8 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlin.io.path.Path
 
+// todo: instead of this, I think gdscript plugin should index everything Godot-releated under the project.godot folder
+
 class GodotFilesIndexingRuleProvider : RiderFilesIndexingRuleProvider {
     override suspend fun collectRules(project: Project): List<RiderFilesIndexingRule> {
         // for some obscure reason, adding indexing rules to a virtual solution would exclude them from Solution scope of the GotoFile.
@@ -32,7 +34,9 @@ class GodotFilesIndexingRuleProvider : RiderFilesIndexingRuleProvider {
 
         val rule = RiderFilesIndexingRule({
             if (!it.isFile) return@RiderFilesIndexingRule null
-            if (it.name.endsWith(".gd", true) && VfsUtil.isAncestor(projectBaseFile, it, true))
+            if ((it.name.endsWith(".gd", true)
+                    || it.name.endsWith(".tscn", true)) // https://youtrack.jetbrains.com/issue/RIDER-131262/Rider-Godots-scene-preview-doesnt-work-in-c.#focus=Comments-27-13239615.0-0
+                && VfsUtil.isAncestor(projectBaseFile, it, true))
                 it
             else
                 null
