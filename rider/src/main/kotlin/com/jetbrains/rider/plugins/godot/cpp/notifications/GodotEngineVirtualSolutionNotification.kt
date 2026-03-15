164f4d8e6efda8a889634a8b2f8654a63f9bd1ab
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-03-15 10:18:47 +0100

    foolowup to fix RIDER-136858 Extract GenerateGodotSolutionAction as an independent action
    GitOrigin-RevId: 6bd2870b1f3e10f4c1c57c58031a0f375f09d49c

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/cpp/notifications/GodotEngineVirtualSolutionNotification.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/cpp/notifications/GodotEngineVirtualSolutionNotification.kt
@@ -1,36 +1,31 @@
-package com.jetbrains.rider.plugins.godot.notifications
+package com.jetbrains.rider.plugins.godot.cpp.notifications
 
 import com.intellij.notification.Notification
 import com.intellij.notification.NotificationAction
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.ActionUpdateThread
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.EDT
-import com.intellij.openapi.application.writeAction
 import com.intellij.openapi.application.writeIntentReadAction
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ex.ProjectManagerEx
 import com.intellij.openapi.startup.ProjectActivity
 import com.intellij.openapi.vfs.refreshAndFindVirtualFile
 import com.jetbrains.rd.ide.model.RdVirtualSolution
-import com.jetbrains.rd.platform.protocol.EdtScheduler
 import com.jetbrains.rd.util.lifetime.Lifetime
 import com.jetbrains.rd.util.reactive.whenTrue
 import com.jetbrains.rd.util.threading.coroutines.launch
 import com.jetbrains.rider.plugins.godot.GodotPluginBundle
 import com.jetbrains.rider.plugins.godot.GodotProjectLifetimeService
-import com.jetbrains.rider.plugins.godot.actions.GenerateGodotSolutionAction
+import com.jetbrains.rider.plugins.godot.cpp.GodotEngineSolutionAvailability
 import com.jetbrains.rider.projectView.SolutionManager
 import com.jetbrains.rider.projectView.solution
 import com.jetbrains.rider.projectView.solutionDescription
 import com.jetbrains.rider.projectView.solutionDirectoryPath
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlin.io.path.exists
-import kotlin.io.path.nameWithoutExtension
 
-class GodotVirtualSolutionNotification : ProjectActivity {
+class GodotEngineVirtualSolutionNotification : ProjectActivity {
 
     companion object {
         private const val GROUP_ID = "GodotVirtualSolutionNotificationGroupId"
@@ -44,8 +39,7 @@ class GodotVirtualSolutionNotification : ProjectActivity {
             project.solution.isLoaded.whenTrue(lifetime) { _ ->
                 val solutionDescription = project.solutionDescription
                 if (solutionDescription is RdVirtualSolution
-                    && (ActionManager.getInstance().getAction("Godot.GenerateSolution") as GenerateGodotSolutionAction)
-                        .isVisible(project) ?: false
+                    && (GodotEngineSolutionAvailability.getInstance(project).isAvailable())
                 ) {
                     showNotification(project)
                 }
