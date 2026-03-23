f2e09e419b5a2b5313e95b25597c8c1a7ca8b8d3
Mario San Roman Caraza <mario.caraza@jetbrains.com>
2026-03-23 18:18:25 +0100

    RIDER-140303 Godot: partial implementation of PolySymbol support for GDScript
    Add support for GDExtensions through the implementation of Poly Symbols. We create the XML files through the use of the 'doctool' run configuration.
    
    Add Completion Contributors and some more PSI symbols.
    
    The main SDK (version SDK) is output to the plugin directory to be shared between projects, and the extensions are output to the .idea folder in each project.
    The stamp system is not yet implemented correctly and I have only tested it with one version so far (4.5).
    
    GitOrigin-RevId: a265c092675c6ffa1ebde070be262f0f1e073c53

---
+++ gdscript/src/main/kotlin/gdscript/polySymbols/completion/GdPolySymbolCompletionItems.kt
@@ -0,0 +1,60 @@
+package gdscript.polySymbols.completion
+
+import com.intellij.polySymbols.PolySymbol
+import com.intellij.polySymbols.PolySymbolQualifiedName
+import com.intellij.polySymbols.completion.PolySymbolCodeCompletionItem
+import com.intellij.polySymbols.query.PolySymbolCodeCompletionQueryParams
+import com.intellij.polySymbols.query.PolySymbolListSymbolsQueryParams
+import com.intellij.polySymbols.query.PolySymbolQueryStack
+import com.intellij.polySymbols.query.PolySymbolScope
+import gdscript.polySymbols.GdClassSymbol
+import gdscript.polySymbols.GdPolySymbol
+import gdscript.polySymbols.GdPolySymbolsConstants
+import gdscript.polySymbols.sdk.GdSdkPolySymbol
+import gdscript.polySymbols.sdk.xml.GdSdkData
+
+object GdPolySymbolPriorities {
+    val BUILT_IN: PolySymbol.Priority = PolySymbol.Priority.custom(10.0)
+    val USER_DEFINED: PolySymbol.Priority = PolySymbol.Priority.custom(20.0)
+    val LOCAL_USER_DEFINED: PolySymbol.Priority = PolySymbol.Priority.custom(25.0)
+}
+
+/** Renders an SDK parameter list as a `(name: Type, …)` hint, matching the PSI completion tail format. */
+fun List<GdSdkData.ParameterData>.toCompletionParamHint(variadic: Boolean = false): String {
+    if (variadic) return "(...)"
+    if (isEmpty()) return "()"
+    return joinToString(", ", "(", ")") {
+        val type = it.type.enumName?.takeIf { name -> name.isNotEmpty() } ?: it.type.name
+        "${it.name}: $type"
+    }
+}
+
+fun GdSdkData.TypeData.toCompletionTypeText(): String =
+    enumName?.takeIf { it.isNotEmpty() } ?: name
+
+fun GdPolySymbol.toCodeCompletionItem(name: String = this.name): PolySymbolCodeCompletionItem =
+    PolySymbolCodeCompletionItem.create(name, symbol = this) {
+        icon(this@toCodeCompletionItem.icon)
+        priority(this@toCodeCompletionItem.priority)
+        tailText(this@toCodeCompletionItem.completionTailText)
+        typeText(this@toCodeCompletionItem.completionTypeText)
+    }
+
+
+fun PolySymbolScope.gdCodeCompletions(
+    qualifiedName: PolySymbolQualifiedName,
+    params: PolySymbolCodeCompletionQueryParams,
+    stack: PolySymbolQueryStack,
+): List<PolySymbolCodeCompletionItem> =
+    getSymbols(
+        qualifiedName.kind,
+        PolySymbolListSymbolsQueryParams.create(params.queryExecutor, expandPatterns = false),
+        stack,
+    )
+        // we dont add PSI results because PSI isn't fully implemented with Poly Symbols and the PSI implementation should handle it for now.
+        .filterIsInstance<GdSdkPolySymbol>() // TODO move to GdPolySymbol when we want to add PSI results as well
+        .map { it.toCodeCompletionItem() }
+
+fun PolySymbolCodeCompletionItem.shouldShow(): Boolean{
+    return !(this.symbol is GdClassSymbol && GdPolySymbolsConstants.GLOBAL_CLASSES.contains(this.name))
+}
