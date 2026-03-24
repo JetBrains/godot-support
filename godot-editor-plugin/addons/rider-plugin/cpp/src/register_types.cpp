792d231bc179e712982b9fd1be0f381004d7a48e
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-03-24 11:00:52 +0100

    add a folder in the root of editor addon zip - fix RIDER-136620

--- godot-editor-plugin/addons/rider-plugin/cpp/src/register_types.cpp
+++ godot-editor-plugin/addons/rider-plugin/cpp/src/register_types.cpp
@@ -22,7 +22,7 @@ void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
 extern "C"
 {
 	// Initialization
-	GDExtensionBool GDE_EXPORT example_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
+	GDExtensionBool GDE_EXPORT rider_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization)
 	{
 		GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
 		init_obj.register_initializer(initialize_gdextension_types);
