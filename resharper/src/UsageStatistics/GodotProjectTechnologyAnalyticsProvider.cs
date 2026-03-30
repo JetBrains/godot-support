f4c60dba5a3f58f41c2bf1ead4666a49cf57ba28
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-03-30 16:16:23 +0200

    fix RIDER-136490 stats about Godot/GDExtensions C++ with both MSBuild/CMake
    (cherry picked from commit 781aa6cb5fa820bc5c757158de91249c3e60a150)
    
    IJ-CR-198415
    
    GitOrigin-RevId: 8bfbcce16759ca709d90197d4ef899f638bcd4c2

--- resharper/src/UsageStatistics/GodotProjectTechnologyAnalyticsProvider.cs
+++ resharper/src/UsageStatistics/GodotProjectTechnologyAnalyticsProvider.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using JetBrains.Application.Parts;
 using JetBrains.IDE.UsageStatistics;
 using JetBrains.ProjectModel;
+using JetBrains.ProjectModel.ProjectsHost;
+using JetBrains.ProjectModel.ProjectsHost.Impl;
 using JetBrains.ReSharper.Plugins.Godot.ProjectModel;
 
 namespace JetBrains.ReSharper.Plugins.Godot.UsageStatistics;
@@ -16,5 +18,22 @@ public class GodotProjectTechnologyAnalyticsProvider : IProjectTechnologyAnalyti
             yield return "Godot";
             yield return "GameDev";
         }
+        
+        var solution = project.GetSolution();
+        // Godot Engine source itself
+        if (solution.SolutionFile?.Name.Equals("godot.sln") ?? false)
+        {
+            yield return "Godot";
+            yield return "GameDev";
+            yield return "Godot C++";
+        }
+        
+        // GDExtension with sln
+        if (solution.GetSolutionMark() is SolutionMark && solution.SolutionDirectory.Combine("godot-cpp").ExistsDirectory)
+        {
+            yield return "GDExtension";
+        }
+        
+        // GDExtension with CMake is in CMakeProjectTechnologyAnalyticsProvider
     }
 }
