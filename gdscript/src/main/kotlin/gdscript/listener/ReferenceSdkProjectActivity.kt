19cc3d5f3df2c12bac458f30ce81f8895eb8c67e
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-04-02 09:13:35 +0200

    fix RIDER-137339 Add stats for Godot versions in Godot projects
    (cherry picked from commit 32f8cef46c23a7ff78ca4e49fcd9ed6496491d06)
    
    IJ-CR-199208
    
    GitOrigin-RevId: 05f295b52dd7027e27dd7c5a9553138c9055a8ba

--- gdscript/src/main/kotlin/gdscript/listener/ReferenceSdkProjectActivity.kt
+++ gdscript/src/main/kotlin/gdscript/listener/ReferenceSdkProjectActivity.kt
@@ -2,18 +2,29 @@ package gdscript.listener
 
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.jetbrains.rider.godot.community.utils.GodotCommunityUtil
-import gdscript.library.GdLibraryUpdater
+import com.intellij.platform.ide.progress.withBackgroundProgress
+import common.util.GdScriptProjectLifetimeService
+import gdscript.GdScriptBundle
+import gdscript.library.GdLibraryManager
+import gdscript.library.GdProjectGodotService
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.launch
 
 class ReferenceSdkProjectActivity : ProjectActivity {
 
     override suspend fun execute(project: Project) {
         if (project.isDisposed) return
+        GdScriptProjectLifetimeService.getInstance(project).scope.launch {
+            GdProjectGodotService.getInstance(project).projectInfoFlow.filterNotNull().collect { info ->
+                GdScriptProjectLifetimeService.getInstance(project).scope.launch {
+                    withBackgroundProgress(project, GdScriptBundle.message("progress.title.check.gdsdk.for.project")) {
+                        if (project.isDisposed) return@withBackgroundProgress
 
-        val flow = GodotCommunityUtil.getGodotProjectBasePathFlow(project)
-        flow.collect { basePath ->
-            if (basePath == null) return@collect
-            GdLibraryUpdater.getInstance(project).scheduleSkdCheck(basePath)
+                        val sdkPath = GdLibraryManager.extractSdkIfNeeded(info.version)
+                        GdLibraryManager.registerSdkIfNeeded(sdkPath, project)
+                    }
+                }
+            }
         }
     }
 }
---
+++ gdscript/src/main/kotlin/gdscript/statistics/GdProjectUsagesCollector.kt
@@ -0,0 +1,22 @@
+package gdscript.statistics
+
+import com.intellij.internal.statistic.beans.MetricEvent
+import com.intellij.internal.statistic.eventLog.EventLogGroup
+import com.intellij.internal.statistic.eventLog.events.EventFields
+import com.intellij.internal.statistic.service.fus.collectors.ProjectUsagesCollector
+import com.intellij.openapi.project.Project
+import gdscript.library.GdProjectGodotService
+
+class GdProjectUsagesCollector : ProjectUsagesCollector() {
+
+    private val GROUP = EventLogGroup("godot.gdscript.plugin.project", 1)
+    private val GODOT_VERSION = EventFields.StringValidatedByInlineRegexp("godot_version", "[0-9]+\\.[0-9]+|[Mm]aster")
+    private val PROJECT = GROUP.registerEvent("project", GODOT_VERSION)
+
+    override fun getGroup(): EventLogGroup = GROUP
+
+    override fun getMetrics(project: Project): Set<MetricEvent> {
+        val version = GdProjectGodotService.getInstance(project).projectInfoFlow.value?.version ?: return emptySet()
+        return setOf(PROJECT.metric(version))
+    }
+}
