07fb311002ad1f81600fa7a03e40a7b58c6431d1
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-04-09 16:29:41 +0200

    fix RIDER-137817 _BASE_.gd is an exception - causes false positive "Class name does not match filename", also https://github.com/JetBrains/godot-support/issues/526
    (cherry picked from commit ba0e36dc9e813306c2c572b5a2980dca69ea8fc3)
    
    IJ-MR-199684
    
    GitOrigin-RevId: d51f4792cfb3768a3198177c7e38d4905e94423d

--- gdscript/src/main/kotlin/gdscript/utils/StringUtil.kt
+++ gdscript/src/main/kotlin/gdscript/utils/StringUtil.kt
@@ -1,42 +1,12 @@
 package gdscript.utils
 
+import com.intellij.polySymbols.utils.NameCaseUtils
 import gdscript.GdKeywords
-import java.util.Locale
 
 object StringUtil {
 
-    private val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
-    private val snakeRegex = "([_ ])[a-zA-Z]".toRegex()
-
-    fun String.camelToSnakeCase(): String {
-        var toLower = true
-        val parsed = this.map {
-            if (toLower) {
-                toLower = it == it.uppercaseChar()
-                it.lowercase()
-            } else {
-                it.toString()
-            }
-        }.joinToString("")
-
-        return camelRegex.replace(parsed) {
-                "_${it.value}"
-            }.lowercase(Locale.getDefault())
-    }
-
-    fun String.snakeToCamelCase(): String {
-        return snakeRegex.replace(this) {
-            it.value.replace("_","")
-                .uppercase(Locale.getDefault())
-        }.replace(" ", "")
-    }
-
-    fun String.snakeToPascalCase(): String {
-        return this
-            .replace("-", "_")
-            .snakeToCamelCase()
-            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
-    }
+    fun String.camelToSnakeCase(): String = NameCaseUtils.toSnakeCase(this)
+    fun String.snakeToPascalCase(): String = NameCaseUtils.toPascalCase(this)
 
     fun List<String>.filterGdTscn(): Array<String> {
         return this.filter { it.endsWith(".gd") || it.endsWith(".tscn") }.toTypedArray()
