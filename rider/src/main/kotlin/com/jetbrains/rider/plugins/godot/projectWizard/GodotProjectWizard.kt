96ba47cc156a7d1b597e7c0d06d695fbcb49f394
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-05-09 14:19:15 +0200

    fix RIDER-129945 Godot Asset Store addon template
    GitOrigin-RevId: 91f2d398cfad67b4218e6448259917e69d8f325f

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/projectWizard/GodotProjectWizard.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/projectWizard/GodotProjectWizard.kt
@@ -13,17 +13,17 @@ import com.jetbrains.rider.projectView.projectTemplates.wizardTemplates.common.R
 import com.jetbrains.rider.projectView.projectTemplates.wizardTemplates.common.riderNewProjectWizardBaseStep
 import javax.swing.Icon
 
-class GodotGameExtensionWizard : GeneratorNewProjectWizard, RiderProjectWizardMode {
+class GodotProjectWizard : GeneratorNewProjectWizard, RiderProjectWizardMode {
 
-    override val id: String = "Rider.GodotGameExtension"
+    override val id: String = "Rider.GodotProject"
 
-    override val name: String = GodotPluginBundle.message("wizard.godot.game.extension.name")
+    override val name: String = GodotPluginBundle.message("wizard.godot.project.name")
 
     override val icon: Icon = GodotIcons.Icons.GodotLogoGray
 
-    override val ordinal: Int = 201
+    override val ordinal: Int = 200
 
-    override val description: String = GodotPluginBundle.message("wizard.godot.game.extension.description")
+    override val description: String = GodotPluginBundle.message("wizard.godot.project.description")
 
     override val groupName: String = GodotPluginBundle.message("wizard.godot.editor.extension.group")
 
@@ -31,7 +31,8 @@ class GodotGameExtensionWizard : GeneratorNewProjectWizard, RiderProjectWizardMo
 
     override fun createStep(context: WizardContext): NewProjectWizardStep =
         RootNewProjectWizardStep(context)
-            .nextStep { riderNewProjectWizardBaseStep(it).also { step -> step.defaultName = "MyGameExtension" } }
+            .nextStep { riderNewProjectWizardBaseStep(it).also { step ->
+                step.defaultName = GodotPluginBundle.message("wizard.godot.project.name").replace(" ", "-") } }
             .nextStep { RiderGitNewProjectWizardStep(it, defaultEnabled = true) }
-            .nextStep(::GodotGameExtensionAssetsStep)
+            .nextStep(::GodotProjectAssetsStep)
 }
