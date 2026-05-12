75f01915e394752ed9a2197fcefaf8495d4cfeac
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-05-12 16:44:15 +0200

    fix RIDER-137853 Open Rider Godot Plugins when starting with a c# file in Godot Editor
    (cherry picked from commit 4f41aa6bd5d39fba85489d24de9e17dc6d0eec05)
    
    IJ-CR-204066
    
    GitOrigin-RevId: db2071e50df2b461b357f85aeeddfe80a9d44fe2

--- resharper/src/ProjectModel/GodotTracker.cs
+++ resharper/src/ProjectModel/GodotTracker.cs
@@ -1,79 +1,60 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Application.Parts;
-using JetBrains.Application.Threading;
-using JetBrains.Application.Threading.Tasks;
 using JetBrains.Lifetimes;
 using JetBrains.ProjectModel;
-using JetBrains.ProjectModel.Tasks;
 using JetBrains.ProjectModel.Tasks.Listeners;
 using JetBrains.Rd.Base;
 using JetBrains.ReSharper.Feature.Services.Protocol;
 using JetBrains.ReSharper.Features.Running;
 using JetBrains.ReSharper.Resources.Shell;
 using JetBrains.Rider.Model.Godot.FrontendBackend;
-using JetBrains.Threading;
 using JetBrains.Util;
-using JetBrains.Util.Threading.Tasks;
 
 namespace JetBrains.ReSharper.Plugins.Godot.ProjectModel
 {
     [SolutionComponent(Instantiation.DemandAnyThreadSafe)]
     public class GodotTracker(
         ISolution solution,
-        ILogger logger,
-        ISolutionLoadTasksScheduler loadTasksScheduler,
-        IShellLocks locks,
-        Lifetime lifetime)
-        : ISolutionLoadTasksInitialSynchronizeSolutionListener2
+        ILogger logger)
+        : ISolutionLoadTasksSolutionStructureReadyListener2
     {
         public VirtualFileSystemPath? MainProjectBasePath { get; private set; }
         public VirtualFileSystemPath? ProjectGodotPath { get; private set; }
-        
+
         public IProject? MainProject { get; private set; }
         public GodotDescriptor? GodotDescriptor { get; private set; }
 
-        IEnumerable<SolutionLoadTasksListenerExecutionStep> ISolutionLoadTasksInitialSynchronizeSolutionListener2.OnSolutionLoadInitialSynchronizeSolution(OuterLifetime loadLifetime)
+        IEnumerable<SolutionLoadTasksListenerExecutionStep> ISolutionLoadTasksSolutionStructureReadyListener2.OnSolutionLoadSolutionStructureReady(OuterLifetime loadLifetime)
         {
-            var barrierCookie = loadTasksScheduler.SetTasksBarrier(GetType(), SolutionLoadTaskKinds.Done,
-                "Waiting for Godot solution load tasks");
-            locks.Tasks.StartNew(lifetime, Scheduling.FreeThreaded, TaskPriority.AboveNormal,  () =>
+            yield return SolutionLoadTasksListenerExecutionStep.YieldToBackgroundThread;
+            if (!solution.SolutionFilePath.IsEmpty)
             {
-                var model = solution.GetProtocolSolution().GetGodotFrontendBackendModel();
-                try
+                var project = GetMainGodotProject();
+                if (project != null)
                 {
-                    if (!solution.SolutionFilePath.IsEmpty)
-                    {
-                        var project = GetMainGodotProject();
-                        if (project == null) return;
-                        MainProject = project;
-                        logger.Verbose($"Godot MainProjectBasePath: {MainProjectBasePath}");
-                        GodotDescriptor = new GodotDescriptor(false, MainProjectBasePath!.FullPath, MainProject.ProjectFile?.Location?.FullPath,
-                            MainProject.TargetFrameworkIds.SingleItem().ToRdTargetFrameworkInfo());
+                    MainProject = project;
+                    logger.Verbose($"Godot MainProjectBasePath: {MainProjectBasePath}");
+                    GodotDescriptor = new GodotDescriptor(false, MainProjectBasePath!.FullPath, MainProject.ProjectFile?.Location?.FullPath,
+                        MainProject.TargetFrameworkIds.SingleItem()?.ToRdTargetFrameworkInfo());
+                }
+            }
+            else
+            {
+                var files = solution.SolutionDirectory.GetChildFiles(3, "project.godot", PathSearchFlags.RecurseIntoSubdirectories);
+                var bestMatch = files.OrderBy(it => it.Components.Count()).FirstOrDefault();
+                if (bestMatch != null)
+                {
+                    GodotDescriptor = new GodotDescriptor(true, bestMatch.Directory.FullPath, null, null);
+                    ProjectGodotPath = bestMatch;
+                }
+            }
 
-                    }
-                    else
-                    {
-                        var files = solution.SolutionDirectory.GetChildFiles(3, "project.godot",
-                            PathSearchFlags.RecurseIntoSubdirectories);
-                        var bestMatch = files.OrderBy(it => it.Components.Count()).FirstOrDefault();
-                        if (bestMatch == null) return;
-                        GodotDescriptor = new GodotDescriptor(true, bestMatch.Directory.FullPath, null, null);
-                        ProjectGodotPath = bestMatch;
-                    }
-                }
-                finally
-                {
-                    barrierCookie.Dispose();
-                    locks.ExecuteOrQueue("Sync Godot model", () =>
-                    {
-                        if (GodotDescriptor != null) 
-                            model.GodotDescriptor.SetValue(GodotDescriptor);
-                        model.IsGodotProject.SetValue(GodotDescriptor != null);
-                    });
-                }
-            }).NoAwait();
-            return EmptyList<SolutionLoadTasksListenerExecutionStep>.Enumerable;
+            yield return SolutionLoadTasksListenerExecutionStep.YieldToMainThreadGuarded;
+            var model = solution.GetProtocolSolution().GetGodotFrontendBackendModel();
+            if (GodotDescriptor != null)
+                model.GodotDescriptor.SetValue(GodotDescriptor);
+            model.IsGodotProject.SetValue(GodotDescriptor != null);
         }
 
         private IProject? GetMainGodotProject()
