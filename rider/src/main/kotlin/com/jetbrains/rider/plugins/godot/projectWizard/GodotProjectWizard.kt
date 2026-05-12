2962a33845d54aa2045697adcfc302c5239691fe
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-05-12 10:20:00 +0200

    fix RIDER-138818 Move Godot project template -> Game Development group
    (cherry picked from commit 9fd39b8940413390ebb6a36491a867fb9379d658)
    
    IJ-CR-203912
    
    GitOrigin-RevId: fdd95cba8a22ea3529a53b21f6ad18a2ae9da0c2

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/projectWizard/GodotProjectWizard.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/projectWizard/GodotProjectWizard.kt
@@ -1,13 +1,13 @@
 package com.jetbrains.rider.plugins.godot.projectWizard
 
 import com.intellij.ide.util.projectWizard.WizardContext
-import com.intellij.ide.wizard.GeneratorNewProjectWizard
 import com.intellij.ide.wizard.NewProjectWizardChainStep.Companion.nextStep
 import com.intellij.ide.wizard.NewProjectWizardStep
 import com.intellij.ide.wizard.RootNewProjectWizardStep
 import com.jetbrains.rider.plugins.godot.GodotIcons
 import com.jetbrains.rider.plugins.godot.GodotPluginBundle
 import com.jetbrains.rider.projectView.projectTemplates.ProjectTemplatesCollector
+import com.jetbrains.rider.projectView.projectTemplates.templateTypes.GameDevTemplateType
 import com.jetbrains.rider.projectView.projectTemplates.wizardTemplates.adapter.RiderProjectWizardMode
 import com.jetbrains.rider.projectView.projectTemplates.wizardTemplates.adapter.RiderWizardFusInfo
 import com.jetbrains.rider.projectView.projectTemplates.wizardTemplates.adapter.WizardMode
@@ -15,7 +15,7 @@ import com.jetbrains.rider.projectView.projectTemplates.wizardTemplates.common.R
 import com.jetbrains.rider.projectView.projectTemplates.wizardTemplates.common.riderNewProjectWizardBaseStep
 import javax.swing.Icon
 
-class GodotProjectWizard : GeneratorNewProjectWizard, RiderProjectWizardMode, RiderWizardFusInfo {
+class GodotProjectWizard : RiderProjectWizardMode, RiderWizardFusInfo, GameDevTemplateType {
 
     override val id: String = "Rider.GodotProject"
 
@@ -23,15 +23,11 @@ class GodotProjectWizard : GeneratorNewProjectWizard, RiderProjectWizardMode, Ri
 
     override val icon: Icon = GodotIcons.Icons.GodotLogoGray
 
-    override val ordinal: Int = 200
-
     override val description: String = GodotPluginBundle.message("wizard.godot.project.description")
-
-    override val groupName: String = GodotPluginBundle.message("wizard.godot.editor.extension.group")
 
     override val wizardMode: WizardMode = WizardMode.NEW_SOLUTION_ONLY
 
-    override val fusProjectType = ProjectTemplatesCollector.ProjectType.Godot
+    override val fusProjectType: ProjectTemplatesCollector.ProjectType = ProjectTemplatesCollector.ProjectType.Godot
 
     override fun createStep(context: WizardContext): NewProjectWizardStep =
         RootNewProjectWizardStep(context)
