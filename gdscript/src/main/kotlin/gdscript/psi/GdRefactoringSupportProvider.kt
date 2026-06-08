66c09f565c3ac367b0da7d08707753d40ad9dbb3
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-06-08 07:48:00 +0200

    workaround RIDER-134345 GDScript - inplace rename refactor doesn't work
    (cherry picked from commit a6c48a019321d1a01d81638a9cc932d2d459dd1f)
    
    IJ-CR-207366
    
    GitOrigin-RevId: 7722d8ced0a4c975d583d069f59a639c8ece7fc2

--- gdscript/src/main/kotlin/gdscript/psi/GdRefactoringSupportProvider.kt
+++ gdscript/src/main/kotlin/gdscript/psi/GdRefactoringSupportProvider.kt
@@ -2,15 +2,15 @@ package gdscript.psi
 
 import com.intellij.lang.refactoring.RefactoringSupportProvider
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.elementType
 import com.intellij.refactoring.RefactoringActionHandler
 import gdscript.refactoring.introduce.GdIntroduceVariableHandler
 
 class GdRefactoringSupportProvider : RefactoringSupportProvider() {
 
-    override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
-        return element.elementType is GdElementType
-    }
+//    RIDER-134345 GDScript - inplace rename refactor doesn't work
+//    override fun isMemberInplaceRenameAvailable(element: PsiElement, context: PsiElement?): Boolean {
+//        return element.elementType is GdElementType
+//    }
 
     override fun isInplaceIntroduceAvailable(element: PsiElement, context: PsiElement?): Boolean {
         return element is GdExpr
--- gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/refactoring/rename/RenameTest.kt
+++ gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/refactoring/rename/RenameTest.kt
@@ -1,6 +1,6 @@
 package com.jetbrains.godot.gdscript.refactoring.rename
 
-import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiDocumentManager
 import com.jetbrains.godot.gdscript.resolve.ResolveTestBase
 import com.jetbrains.godot.getBaseTestDataPath
 import kotlin.io.path.pathString
@@ -13,21 +13,21 @@ class RenameTest : ResolveTestBase() {
 
     fun testRenameVariable() {
         val code = """
-func in_the_outer_wrong():
-	var <caret>A1Instance := A1.new()
-	renamed_A1Instance.ppa1() # will be updated after rename
-
-class A1:
-	func ppa1():
-		pass
-        """.trimIndent()
+            |func in_the_outer_wrong():
+            |	var <caret>A1Instance := A1.new()
+            |	renamed_A1Instance.ppa1() # will be updated after rename
+            |
+            |class A1:
+            |	func ppa1():
+            |		pass
+        """.trimMargin()
 
         // Configure the file from text and perform rename at caret
-        val file: PsiFile = myFixture.configureByText("rename_var.gd", code)
+        myFixture.configureByText("rename_var.gd", code)
         myFixture.renameElementAtCaret("renamed_A1Instance")
 
         // Commit and run highlighting to simulate IDE analysis (and warm caches)
-        com.intellij.psi.PsiDocumentManager.getInstance(project).commitAllDocuments()
+        PsiDocumentManager.getInstance(project).commitAllDocuments()
         myFixture.doHighlighting() // forces resolve/indices/inspections
 
         // Verify the text was updated accordingly
@@ -38,5 +38,23 @@ class A1:
         // Dump resolves and assert there are no unresolved references
         val dump = dumpResolvesWithInlineMarkers(myFixture.file)
         assertGold(dump)
+    }
+
+    fun testRenameVariableShortName() {
+        val code = """
+        |    func _ready() -> void:
+	    |        var test_long_name := 1
+	    |        print(test_long<caret>_name)
+        """.trimMargin()
+
+        myFixture.configureByText("rename_var_short.gd", code)
+        myFixture.renameElementAtCaret("test_name")
+
+        PsiDocumentManager.getInstance(project).commitAllDocuments()
+        myFixture.doHighlighting()
+
+        val updated = myFixture.file.text
+        assertTrue("expected 'var test_name := 1' but got:\n$updated", updated.contains("var test_name := 1"))
+        assertTrue("expected 'print(test_name)' but got:\n$updated", updated.contains("print(test_name)"))
     }
 }
