3092448a8ee37c90bb1fb9e6889e339eb0cc5282
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-14 16:45:18 +0200

    RIDER-140303 Godot: add missing resolve for static access qualifiers
    GitOrigin-RevId: 5c31f26747a8aa8aba371b66573f15bc135bc6b0

--- gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiPropertySymbol.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiPropertySymbol.kt
@@ -5,8 +5,10 @@ import com.intellij.model.Pointer
 import com.intellij.openapi.project.Project
 import com.intellij.polySymbols.PolySymbol
 import com.intellij.polySymbols.PolySymbolKind
+import com.intellij.polySymbols.PolySymbolModifier
 import com.intellij.psi.createSmartPointer
 import gdscript.polySymbols.GdPolySymbolKind
+import gdscript.polySymbols.GdPolySymbolModifier
 import gdscript.polySymbols.completion.GdPolySymbolPriorities
 import gdscript.psi.GdClassVarDeclTl
 import gdscript.psi.GdVarNmi
@@ -24,6 +26,9 @@ class GdPsiPropertySymbol(
     override val icon: Icon get() = GdScriptPluginIcons.GDScriptIcons.VAR_MARKER
     override val priority: PolySymbol.Priority get() = GdPolySymbolPriorities.USER_DEFINED
     override val completionTypeText: String? get() = (linkedElement.parent as? GdClassVarDeclTl)?.returnType?.takeIf { it.isNotEmpty() }
+
+    override val modifiers: Set<PolySymbolModifier>
+        get() = if ((linkedElement.parent as? GdClassVarDeclTl)?.isStatic == true) setOf(GdPolySymbolModifier.STATIC) else emptySet()
 
     override fun createPointer(): Pointer<out GdPsiPropertySymbol> {
         val sourcePtr = linkedElement.createSmartPointer()
