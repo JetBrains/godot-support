eec9628673161eb1dcc775f6a1e989adb3a2b369
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-14 17:27:45 +0200

    RIDER-140303 Godot: migrate GdEnumNavigationTest
    GitOrigin-RevId: 7f0fcc64f13627ce115b27d588be193a62718a00

--- gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/redCode/GdEnumNavigationTest.kt
+++ gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/redCode/GdEnumNavigationTest.kt
@@ -1,30 +1,19 @@
 package com.jetbrains.godot.gdscript.redCode
 
-import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.polySymbols.testFramework.checkGotoDeclaration
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
-import gdscript.psi.GdRefIdRef
 
 class GdEnumNavigationTest : BasePlatformTestCase() {
-  fun testEnumMemberResolvesToEnumValue() {
-    val code = """
-      |enum _Anim {
-      |  FLOOR,
-      |  AIR,
-      |}
-      |
-      |var anim : _Anim = _Anim.FLOOR
-      |""".trimMargin()
-
-    val psiFile = myFixture.configureByText("a.gd", code)
-
-    val refs = PsiTreeUtil.collectElementsOfType(psiFile, GdRefIdRef::class.java)
-    val target = refs.last { it.text == "FLOOR" }
-
-    val ref = target.references.firstOrNull()
-    assertNotNull("Expected a reference on enum member usage", ref)
-
-    val resolved = ref!!.resolve()
-    assertNotNull("Enum member reference should resolve to its declaration", resolved)
-    assertEquals("FLOOR", resolved!!.text)
-  }
+    fun testEnumMemberResolvesToEnumValue() {
+        val code = """
+          |enum _Anim {
+          |  FLOOR,
+          |  AIR,
+          |}
+          |
+          |var anim : _Anim = _Anim.FLOOR
+          |""".trimMargin()
+        myFixture.configureByText("a.gd", code)
+        myFixture.checkGotoDeclaration("_Anim.FL<caret>OOR", "<caret>FLOOR,")
+    }
 }
