47d4237ae67f726c5cb318256f8d263a07b256fa
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-14 18:44:07 +0200

    RIDER-140303 Godot: remove "PolySymbols" name from tests names and remove polySymbols directory
    GitOrigin-RevId: 1b23d583dca4f6a88196ab69ef8e9ee03a968933

--- gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/reference/GdRefIdReferenceTest.kt
+++ gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/reference/GdRefIdReferenceTest.kt
@@ -1,6 +1,6 @@
-package com.jetbrains.godot.gdscript.polySymbols.reference
+package com.jetbrains.godot.gdscript.reference
 
-import com.jetbrains.godot.gdscript.polySymbols.GdPolySymbolsTestCaseWithSdk
+import com.jetbrains.godot.gdscript.GdTestCaseWithSdk
 import gdscript.polySymbols.sdk.GdSdkMethodSymbol
 import gdscript.polySymbols.sdk.GdSdkPropertySymbol
 import org.junit.Test
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
 @RunWith(JUnit4::class)
-class GdRefIdPolySymbolReferenceTest : GdPolySymbolsTestCaseWithSdk("reference") {
+class GdRefIdReferenceTest : GdTestCaseWithSdk("reference") {
 
     @Test
     fun testResolveClassMemberSdkProperty() =
