46886ee3da747ad3f151b60cc055733a41ed54c2
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 22:09:48 +0200

    RIDER-140303 GDScript: remove PsiPolySymbolReferenceProvider and switch to own references on GdRefElements
    GitOrigin-RevId: 8bcc004c880841bdd79674f2467120f920131082

--- gdscript/src/main/kotlin/gdscript/psi/impl/GdTypeHintRefImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdTypeHintRefImpl.kt
@@ -1,11 +1,14 @@
 package gdscript.psi.impl
 
 import com.intellij.lang.ASTNode
+import com.intellij.polySymbols.references.PolySymbolOwnReferences
+import com.intellij.polySymbols.references.PolySymbolOwnReferencesHost
 import com.intellij.psi.PsiElementVisitor
+import gdscript.polySymbols.GdPolySymbolKind.TYPE_HINTS
 import gdscript.psi.GdTypeHintRef
 import gdscript.psi.GdVisitor
 
-class GdTypeHintRefImpl(node: ASTNode) : GdRefElementImpl(node), GdTypeHintRef {
+class GdTypeHintRefImpl(node: ASTNode) : GdRefElementImpl(node), GdTypeHintRef, PolySymbolOwnReferencesHost {
     fun accept(visitor: GdVisitor) {
         visitor.visitTypeHintRef(this)
     }
@@ -13,5 +16,9 @@ class GdTypeHintRefImpl(node: ASTNode) : GdRefElementImpl(node), GdTypeHintRef {
     override fun accept(visitor: PsiElementVisitor) {
         if (visitor is GdVisitor) accept(visitor)
         else super.accept(visitor)
+    }
+
+    override fun buildOwnReferences(builder: PolySymbolOwnReferences.Builder) {
+        builder.fromNameMatchQuery(TYPE_HINTS, text)
     }
 }
