50abc5a66737a10200ef3384a158a24c38d5bd24
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 17:14:06 +0200

    RIDER-140303 GDScript: migrate GdRefIdRef to use PolySymbolOwnReferences
    GitOrigin-RevId: 004003a44c4da202e0224d1c8b96bc464c7acfa9

--- gdscript/src/main/java/gdscript/psi/GdRefIdRef.java
+++ gdscript/src/main/java/gdscript/psi/GdRefIdRef.java
@@ -1,5 +1,21 @@
 package gdscript.psi;
 
-public interface GdRefIdRef extends GdRefElement {
+import com.intellij.polySymbols.references.PolySymbolOwnReferences;
+import com.intellij.polySymbols.references.PolySymbolOwnReferencesHost;
+import gdscript.polySymbols.GdPolySymbolKind;
+import gdscript.polySymbols.GdPolySymbolModifier;
+import gdscript.polySymbols.psi.GdPsiPolySymbolUtil;
+import org.jetbrains.annotations.NotNull;
 
+public interface GdRefIdRef extends GdRefElement, PolySymbolOwnReferencesHost {
+
+    @Override
+    default void buildOwnReferences(@NotNull PolySymbolOwnReferences.Builder builder) {
+        builder.fromNameMatchQuery(
+            GdPolySymbolKind.INSTANCE.getQUALIFIABLE_SYMBOLS(), getText(),
+            GdPsiPolySymbolUtil.INSTANCE.isStatic(this)
+            ? (symbol) -> symbol.getModifiers().contains(GdPolySymbolModifier.INSTANCE.getSTATIC())
+            : (_) -> true
+        );
+    }
 }
