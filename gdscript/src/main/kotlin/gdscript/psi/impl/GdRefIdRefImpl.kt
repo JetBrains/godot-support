ed1234fd948893edb9e62623852da412f58b3cd3
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 22:09:48 +0200

    RIDER-140303 GDScript: remove PsiPolySymbolReferenceProvider and switch to own references on GdRefElements
    GitOrigin-RevId: 8bcc004c880841bdd79674f2467120f920131082

--- gdscript/src/main/kotlin/gdscript/psi/impl/GdRefIdRefImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdRefIdRefImpl.kt
@@ -1,11 +1,19 @@
 package gdscript.psi.impl
 
 import com.intellij.lang.ASTNode
+import com.intellij.polySymbols.references.PolySymbolOwnReferences
+import com.intellij.polySymbols.references.PolySymbolOwnReferencesHost
 import com.intellij.psi.PsiElementVisitor
+import gdscript.polySymbols.GdPolySymbolKind.QUALIFIABLE_SYMBOLS
+import gdscript.polySymbols.GdPolySymbolModifier.STATIC
+import gdscript.polySymbols.psi.GdPsiPolySymbolUtil.isStatic
+import gdscript.polySymbols.psi.GdPsiPolySymbolUtil.resolveConstructorSymbol
+import gdscript.polySymbols.scope.hasModifier
+import gdscript.polySymbols.scope.hasStaticInstanceDistinction
 import gdscript.psi.GdRefIdRef
 import gdscript.psi.GdVisitor
 
-class GdRefIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdRefIdRef {
+class GdRefIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdRefIdRef, PolySymbolOwnReferencesHost {
     fun accept(visitor: GdVisitor) {
         visitor.visitRefIdNm(this)
     }
@@ -13,5 +21,16 @@ class GdRefIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdRefIdRef {
     override fun accept(visitor: PsiElementVisitor) {
         if (visitor is GdVisitor) accept(visitor)
         else super.accept(visitor)
+    }
+
+    override fun buildOwnReferences(builder: PolySymbolOwnReferences.Builder) {
+        if (text == "new") {
+            resolveConstructorSymbol(this)?.let { builder.reference(it) }
+            return
+        }
+        val requireStatic = isStatic(this)
+        builder.fromNameMatchQuery(QUALIFIABLE_SYMBOLS, text) { symbol ->
+            !requireStatic || !symbol.hasStaticInstanceDistinction() || symbol.hasModifier(STATIC)
+        }
     }
 }
