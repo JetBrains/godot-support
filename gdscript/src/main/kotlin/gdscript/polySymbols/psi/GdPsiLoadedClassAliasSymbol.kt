7a307bb0ff6b747d76e3dfdfc4f3cc8ca4816e02
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 16:08:09 +0200

    RIDER-140303 Godot: introduce GdPsiPolySymbolDeclarationProvider and migrate symbols to PolySymbolDeclaredInPsi interface
    GitOrigin-RevId: 9582e630051c66d784206dd089070d46d10392bf

--- gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiLoadedClassAliasSymbol.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiLoadedClassAliasSymbol.kt
@@ -2,7 +2,6 @@ package gdscript.polySymbols.psi
 
 import GdScriptPluginIcons
 import com.intellij.model.Pointer
-import com.intellij.openapi.project.Project
 import com.intellij.polySymbols.PolySymbol
 import com.intellij.polySymbols.PolySymbolKind
 import com.intellij.psi.createSmartPointer
@@ -20,25 +19,23 @@ import javax.swing.Icon
  * var x: MyScene  # <- MyScene is resolved via this symbol
  * ```
  *
- * The [linkedElement] is the name-identifier (`GdVarNmi`) of the var/const declaration.
- * Navigate up via [linkedElement].parent to reach the full declaration and resolve the loaded class.
+ * The [sourceElement] is the name-identifier (`GdVarNmi`) of the var/const declaration.
+ * Navigate up via [sourceElement].parent to reach the full declaration and resolve the loaded class.
  *
  * [ownerClassId] identifies the class (or resource file) that owns the alias declaration.
  */
 class GdPsiLoadedClassAliasSymbol(
-    override val linkedElement: GdVarNmi,
+    override val sourceElement: GdVarNmi,
 ) : GdPsiPolySymbol() {
-    override val project: Project get() = linkedElement.project
-
     override val kind: PolySymbolKind get() = GdPolySymbolKind.LOADED_CLASS_ALIAS
-    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(linkedElement)
+    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(sourceElement)
     override val declaringClassName: String get() = GdPsiPolySymbolUtil.getLeafName(declaringClassId)
 
     override val icon: Icon get() = GdScriptPluginIcons.Icons.BackupIcon
     override val priority: PolySymbol.Priority get() = GdPolySymbolPriorities.USER_DEFINED
 
     override fun createPointer(): Pointer<out GdPsiLoadedClassAliasSymbol> {
-        val sourcePtr = linkedElement.createSmartPointer()
+        val sourcePtr = sourceElement.createSmartPointer()
         return Pointer {
             sourcePtr.element?.let { GdPsiLoadedClassAliasSymbol(it) }
         }
--- gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiParameterSymbol.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiParameterSymbol.kt
@@ -2,7 +2,6 @@ package gdscript.polySymbols.psi
 
 import GdScriptPluginIcons
 import com.intellij.model.Pointer
-import com.intellij.openapi.project.Project
 import com.intellij.polySymbols.PolySymbol
 import com.intellij.polySymbols.PolySymbolKind
 import com.intellij.psi.createSmartPointer
@@ -14,17 +13,15 @@ import javax.swing.Icon
 /**
  * Represents a method parameter in GDScript.
  *
- * The [linkedElement] is the [GdVarNmi] name identifier child of [gdscript.psi.GdParam].
+ * The [sourceElement] is the [GdVarNmi] name identifier child of [gdscript.psi.GdParam].
  *
  * It is local, so has no declaring class.
  */
 class GdPsiParameterSymbol(
-    override val linkedElement: GdVarNmi
+    override val sourceElement: GdVarNmi
 ) : GdPsiPolySymbol() {
-    override val project: Project get() = linkedElement.project
-
     override val kind: PolySymbolKind get() = GdPolySymbolKind.PARAMETER
-    override val name: String get() = linkedElement.name
+    override val name: String get() = sourceElement.name
     override val declaringClassName: String get() = ""
     override val declaringClassId: String get() = ""
 
@@ -32,7 +29,7 @@ class GdPsiParameterSymbol(
     override val priority: PolySymbol.Priority get() = GdPolySymbolPriorities.LOCAL_USER_DEFINED
 
     override fun createPointer(): Pointer<out GdPsiParameterSymbol> {
-        val ptr = linkedElement.createSmartPointer()
+        val ptr = sourceElement.createSmartPointer()
         return Pointer { ptr.element?.let { GdPsiParameterSymbol(it) } }
     }
 }
