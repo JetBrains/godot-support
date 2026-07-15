1e78ea7dc8dc8ee3224c60b43467afdc63a85315
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 22:09:48 +0200

    RIDER-140303 GDScript: remove PsiPolySymbolReferenceProvider and switch to own references on GdRefElements
    GitOrigin-RevId: 8bcc004c880841bdd79674f2467120f920131082

--- gdscript/src/main/kotlin/gdscript/psi/GdRefIdRef.kt
+++ gdscript/src/main/kotlin/gdscript/psi/GdRefIdRef.kt
@@ -1,20 +1,3 @@
 package gdscript.psi
 
-import com.intellij.polySymbols.PolySymbol
-import com.intellij.polySymbols.references.PolySymbolOwnReferences
-import com.intellij.polySymbols.references.PolySymbolOwnReferencesHost
-import gdscript.polySymbols.GdPolySymbolKind.QUALIFIABLE_SYMBOLS
-import gdscript.polySymbols.GdPolySymbolModifier.STATIC
-import gdscript.polySymbols.psi.GdPsiPolySymbolUtil.isStatic
-
-interface GdRefIdRef : GdRefElement, PolySymbolOwnReferencesHost {
-    override fun buildOwnReferences(builder: PolySymbolOwnReferences.Builder) {
-        builder.fromNameMatchQuery(
-            QUALIFIABLE_SYMBOLS, getText(),
-            if (isStatic(this))
-                { symbol: PolySymbol? -> symbol!!.modifiers.contains(STATIC) }
-            else
-                { `_`: PolySymbol? -> true }
-        )
-    }
-}
+interface GdRefIdRef : GdRefElement
