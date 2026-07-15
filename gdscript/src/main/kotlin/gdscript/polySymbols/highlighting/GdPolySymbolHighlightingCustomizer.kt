d092e3c82db30260ad00faec35f01269ded455a2
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 22:09:48 +0200

    RIDER-140303 GDScript: remove PsiPolySymbolReferenceProvider and switch to own references on GdRefElements
    GitOrigin-RevId: 8bcc004c880841bdd79674f2467120f920131082

---
+++ gdscript/src/main/kotlin/gdscript/polySymbols/highlighting/GdPolySymbolHighlightingCustomizer.kt
@@ -0,0 +1,59 @@
+package gdscript.polySymbols.highlighting
+
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.polySymbols.PolySymbol
+import com.intellij.polySymbols.highlighting.PolySymbolHighlightingCustomizer
+import com.intellij.polySymbols.utils.unwrapMatchedSymbols
+import com.intellij.psi.PsiElement
+import gdscript.GdKeywords
+import gdscript.highlighter.GdHighlighterColors
+import gdscript.polySymbols.GdPolySymbol
+import gdscript.polySymbols.GdPolySymbolKind
+import gdscript.polySymbols.GdPolySymbolModifier
+import gdscript.polySymbols.sdk.GdSdkPolySymbol
+import gdscript.utils.PsiElementUtil.getCallExpr
+
+class GdPolySymbolHighlightingCustomizer : PolySymbolHighlightingCustomizer {
+
+    override fun getSymbolTextAttributes(host: PsiElement, symbol: PolySymbol, level: Int): TextAttributesKey? {
+        // `symbol` is often a raw nameMatchQuery result wrapper whose own kind/modifiers/etc. don't
+        // reflect the real underlying symbol - unwrap first (same pattern as PolySymbol.hasModifier).
+        val real = symbol.unwrapMatchedSymbols().firstOrNull() as? GdPolySymbol ?: return null
+
+        var attribute = when (real.kind) {
+            GdPolySymbolKind.CONSTRUCTOR -> GdHighlighterColors.METHOD_CALL
+
+            GdPolySymbolKind.METHOD -> {
+                if (real.declaringClassId == GdKeywords.GLOBAL_SCOPE) GdHighlighterColors.GLOBAL_FUNCTION
+                else if (real.modifiers.contains(GdPolySymbolModifier.STATIC)) GdHighlighterColors.STATIC_METHOD_CALL
+                else GdHighlighterColors.METHOD_CALL
+            }
+
+            GdPolySymbolKind.PROPERTY -> {
+                if (real.declaringClassId == GdKeywords.GLOBAL_SCOPE) GdHighlighterColors.GLOBAL_VARIABLE_BUILT_IN
+                else GdHighlighterColors.MEMBER
+            }
+
+            GdPolySymbolKind.CONSTANT,
+            GdPolySymbolKind.SIGNAL,
+            GdPolySymbolKind.ENUM,
+            GdPolySymbolKind.ENUM_VALUE -> GdHighlighterColors.MEMBER
+
+            GdPolySymbolKind.CLASS -> {
+                if (real is GdSdkPolySymbol) GdHighlighterColors.ENGINE_TYPE
+                else GdHighlighterColors.CLASS_TYPE
+            }
+
+            GdPolySymbolKind.AUTOLOAD -> GdHighlighterColors.GLOBAL_VARIABLE_AUTOLOAD
+            GdPolySymbolKind.LOADED_CLASS_ALIAS -> GdHighlighterColors.CLASS_TYPE
+
+            else -> GdHighlighterColors.MEMBER
+        }
+
+        if (attribute == GdHighlighterColors.MEMBER && host.getCallExpr() != null) {
+            attribute = GdHighlighterColors.METHOD_CALL
+        }
+
+        return attribute
+    }
+}
