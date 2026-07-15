bade8308da3dc0a6f1c1a6ddcf5e02a52dad9167
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 16:08:09 +0200

    RIDER-140303 Godot: introduce GdPsiPolySymbolDeclarationProvider and migrate symbols to PolySymbolDeclaredInPsi interface
    GitOrigin-RevId: 9582e630051c66d784206dd089070d46d10392bf

--- gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiConstantSymbol.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiConstantSymbol.kt
@@ -2,7 +2,6 @@ package gdscript.polySymbols.psi
 
 import GdScriptPluginIcons
 import com.intellij.model.Pointer
-import com.intellij.openapi.project.Project
 import com.intellij.polySymbols.PolySymbol
 import com.intellij.polySymbols.PolySymbolKind
 import com.intellij.psi.createSmartPointer
@@ -13,20 +12,18 @@ import gdscript.psi.GdVarNmi
 import javax.swing.Icon
 
 class GdPsiConstantSymbol(
-    override val linkedElement: GdVarNmi,
+    override val sourceElement: GdVarNmi,
 ) : GdPsiPolySymbol() {
-    override val project: Project get() = linkedElement.project
-
     override val kind: PolySymbolKind get() = GdPolySymbolKind.CONSTANT
-    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(linkedElement)
+    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(sourceElement)
     override val declaringClassName: String get() = GdPsiPolySymbolUtil.getLeafName(declaringClassId)
 
     override val icon: Icon get() = GdScriptPluginIcons.GDScriptIcons.CONST_MARKER
     override val priority: PolySymbol.Priority get() = GdPolySymbolPriorities.USER_DEFINED
-    override val completionTypeText: String? get() = (linkedElement.parent as? GdConstDeclTl)?.returnType?.takeIf { it.isNotEmpty() }
+    override val completionTypeText: String? get() = (sourceElement.parent as? GdConstDeclTl)?.returnType?.takeIf { it.isNotEmpty() }
 
     override fun createPointer(): Pointer<out GdPsiConstantSymbol> {
-        val sourcePtr = linkedElement.createSmartPointer()
+        val sourcePtr = sourceElement.createSmartPointer()
         return Pointer {
             sourcePtr.element?.let {
                 GdPsiConstantSymbol(it)
