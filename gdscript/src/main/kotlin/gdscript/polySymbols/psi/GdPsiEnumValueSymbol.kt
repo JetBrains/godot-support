0933c9021f1147971f0b22faaa20db13c93578de
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 16:08:09 +0200

    RIDER-140303 Godot: introduce GdPsiPolySymbolDeclarationProvider and migrate symbols to PolySymbolDeclaredInPsi interface
    GitOrigin-RevId: 9582e630051c66d784206dd089070d46d10392bf

--- gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiEnumValueSymbol.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiEnumValueSymbol.kt
@@ -2,7 +2,6 @@ package gdscript.polySymbols.psi
 
 import GdScriptPluginIcons
 import com.intellij.model.Pointer
-import com.intellij.openapi.project.Project
 import com.intellij.polySymbols.PolySymbol
 import com.intellij.polySymbols.PolySymbolKind
 import com.intellij.psi.createSmartPointer
@@ -19,13 +18,11 @@ import javax.swing.Icon
  * [ownerClassId] identifies the class that owns the enum (not the enum itself).
  */
 class GdPsiEnumValueSymbol(
-    override val linkedElement: GdEnumValueNmi,
+    override val sourceElement: GdEnumValueNmi,
 ) : GdPsiPolySymbol() {
-    override val project: Project get() = linkedElement.project
-
     // TODO should probably add a reference to the enum that owns it, but havent found a need for it yet
     override val kind: PolySymbolKind get() = GdPolySymbolKind.ENUM_VALUE
-    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(linkedElement)
+    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(sourceElement)
     override val declaringClassName: String get() = GdPsiPolySymbolUtil.getLeafName(declaringClassId)
 
     override val icon: Icon get() = GdScriptPluginIcons.GDScriptIcons.ENUM_MARKER
@@ -33,7 +30,7 @@ class GdPsiEnumValueSymbol(
     override val completionTypeText: String get() = GdKeywords.INT
 
     override fun createPointer(): Pointer<out GdPsiEnumValueSymbol> {
-        val sourcePtr = linkedElement.createSmartPointer()
+        val sourcePtr = sourceElement.createSmartPointer()
         return Pointer {
             sourcePtr.element?.let { GdPsiEnumValueSymbol(it) }
         }
