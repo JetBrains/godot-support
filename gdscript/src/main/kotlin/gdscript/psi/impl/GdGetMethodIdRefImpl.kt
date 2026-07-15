42c0a5cbe0a9d5f73b93a538b59323b9af144e35
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 22:09:48 +0200

    RIDER-140303 GDScript: remove PsiPolySymbolReferenceProvider and switch to own references on GdRefElements
    GitOrigin-RevId: 8bcc004c880841bdd79674f2467120f920131082

--- gdscript/src/main/kotlin/gdscript/psi/impl/GdGetMethodIdRefImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdGetMethodIdRefImpl.kt
@@ -1,11 +1,18 @@
 package gdscript.psi.impl
 
 import com.intellij.lang.ASTNode
+import com.intellij.polySymbols.references.PolySymbolOwnReferences
+import com.intellij.polySymbols.references.PolySymbolOwnReferencesHost
 import com.intellij.psi.PsiElementVisitor
+import com.intellij.psi.util.PsiTreeUtil
+import gdscript.polySymbols.GdPolySymbolKind.METHOD
+import gdscript.polySymbols.GdPolySymbolModifier.STATIC
+import gdscript.polySymbols.scope.hasModifier
+import gdscript.psi.GdClassVarDeclTl
 import gdscript.psi.GdGetMethodIdRef
 import gdscript.psi.GdVisitor
 
-class GdGetMethodIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdGetMethodIdRef {
+class GdGetMethodIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdGetMethodIdRef, PolySymbolOwnReferencesHost {
     fun accept(visitor: GdVisitor) {
         visitor.visitGetMethodIdRef(this)
     }
@@ -13,5 +20,12 @@ class GdGetMethodIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdGetMethodI
     override fun accept(visitor: PsiElementVisitor) {
         if (visitor is GdVisitor) accept(visitor)
         else super.accept(visitor)
+    }
+
+    override fun buildOwnReferences(builder: PolySymbolOwnReferences.Builder) {
+        val requireStatic = PsiTreeUtil.getStubOrPsiParentOfType(this, GdClassVarDeclTl::class.java)?.isStatic == true
+        builder.fromNameMatchQuery(METHOD, text) { symbol ->
+            !requireStatic || symbol.hasModifier(STATIC)
+        }
     }
 }
