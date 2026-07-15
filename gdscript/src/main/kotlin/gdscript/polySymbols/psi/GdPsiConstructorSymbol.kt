af159ac56a8c4dd7503628ad162791721efe0d01
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-15 16:08:09 +0200

    RIDER-140303 Godot: introduce GdPsiPolySymbolDeclarationProvider and migrate symbols to PolySymbolDeclaredInPsi interface
    GitOrigin-RevId: 9582e630051c66d784206dd089070d46d10392bf

--- gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiConstructorSymbol.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiConstructorSymbol.kt
@@ -2,7 +2,6 @@ package gdscript.polySymbols.psi
 
 import GdScriptPluginIcons
 import com.intellij.model.Pointer
-import com.intellij.openapi.project.Project
 import com.intellij.polySymbols.PolySymbol
 import com.intellij.polySymbols.PolySymbolKind
 import com.intellij.psi.createSmartPointer
@@ -14,22 +13,20 @@ import gdscript.psi.GdMethodIdNmi
 import javax.swing.Icon
 
 class GdPsiConstructorSymbol(
-    override val linkedElement: GdMethodIdNmi,
+    override val sourceElement: GdMethodIdNmi,
 ) : GdPsiPolySymbol() {
-    override val project: Project get() = linkedElement.project
-
     override val kind: PolySymbolKind get() = GdPolySymbolKind.CONSTRUCTOR
-    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(linkedElement)
+    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(sourceElement)
     override val declaringClassName: String get() = GdPsiPolySymbolUtil.getLeafName(declaringClassId)
     override val returnType: String get() = declaringClassId
 
     override val icon: Icon get() = GdScriptPluginIcons.GDScriptIcons.METHOD_MARKER
     override val priority: PolySymbol.Priority get() = GdPolySymbolPriorities.USER_DEFINED
-    override val completionTailText: String? get() = (linkedElement.parent as? GdMethodDeclTl)?.let { buildParamHint(it) }
+    override val completionTailText: String? get() = (sourceElement.parent as? GdMethodDeclTl)?.let { buildParamHint(it) }
     override val completionTypeText: String get() = declaringClassName
 
     override fun createPointer(): Pointer<out GdPsiConstructorSymbol> {
-        val sourcePtr = linkedElement.createSmartPointer()
+        val sourcePtr = sourceElement.createSmartPointer()
         return Pointer {
             sourcePtr.element?.let {
                 GdPsiConstructorSymbol(it)
--- gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiPropertySymbol.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiPropertySymbol.kt
@@ -2,7 +2,6 @@ package gdscript.polySymbols.psi
 
 import GdScriptPluginIcons
 import com.intellij.model.Pointer
-import com.intellij.openapi.project.Project
 import com.intellij.polySymbols.PolySymbol
 import com.intellij.polySymbols.PolySymbolKind
 import com.intellij.polySymbols.PolySymbolModifier
@@ -15,23 +14,21 @@ import gdscript.psi.GdVarNmi
 import javax.swing.Icon
 
 class GdPsiPropertySymbol(
-    override val linkedElement: GdVarNmi,
+    override val sourceElement: GdVarNmi,
 ) : GdPsiPolySymbol() {
-    override val project: Project get() = linkedElement.project
-
     override val kind: PolySymbolKind get() = GdPolySymbolKind.PROPERTY
-    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(linkedElement)
+    override val declaringClassId: String get() = GdPsiPolySymbolUtil.getOwnerClassId(sourceElement)
     override val declaringClassName: String get() = GdPsiPolySymbolUtil.getLeafName(declaringClassId)
 
     override val icon: Icon get() = GdScriptPluginIcons.GDScriptIcons.VAR_MARKER
     override val priority: PolySymbol.Priority get() = GdPolySymbolPriorities.USER_DEFINED
-    override val completionTypeText: String? get() = (linkedElement.parent as? GdClassVarDeclTl)?.returnType?.takeIf { it.isNotEmpty() }
+    override val completionTypeText: String? get() = (sourceElement.parent as? GdClassVarDeclTl)?.returnType?.takeIf { it.isNotEmpty() }
 
     override val modifiers: Set<PolySymbolModifier>
-        get() = if ((linkedElement.parent as? GdClassVarDeclTl)?.isStatic == true) setOf(GdPolySymbolModifier.STATIC) else emptySet()
+        get() = if ((sourceElement.parent as? GdClassVarDeclTl)?.isStatic == true) setOf(GdPolySymbolModifier.STATIC) else emptySet()
 
     override fun createPointer(): Pointer<out GdPsiPropertySymbol> {
-        val sourcePtr = linkedElement.createSmartPointer()
+        val sourcePtr = sourceElement.createSmartPointer()
         return Pointer {
             sourcePtr.element?.let {
                 GdPsiPropertySymbol(it)
