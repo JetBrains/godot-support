ba344b206887d59bd0e1506b7914ed03e454c594
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-16 13:08:32 +0200

    RIDER-140303 GDScript: WIP on switching to PolySymbols - GdClassMemberReference removed and GDRefElements switched to providing own references
    GitOrigin-RevId: 7f59a2d24ee1c2b1058e30302be3edc41a16a5ce

--- gdscript/src/main/kotlin/gdscript/psi/impl/GdGetMethodIdRefImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdGetMethodIdRefImpl.kt
@@ -1,8 +1,8 @@
 package gdscript.psi.impl
 
 import com.intellij.lang.ASTNode
-import com.intellij.polySymbols.references.PolySymbolOwnReferences
-import com.intellij.polySymbols.references.PolySymbolOwnReferencesHost
+import com.intellij.model.psi.PsiSymbolReference
+import com.intellij.polySymbols.references.polySymbolOwnReferences
 import com.intellij.psi.PsiElementVisitor
 import com.intellij.psi.util.PsiTreeUtil
 import gdscript.polySymbols.GdPolySymbolKind.METHOD
@@ -11,8 +11,9 @@ import gdscript.polySymbols.scope.hasModifier
 import gdscript.psi.GdClassVarDeclTl
 import gdscript.psi.GdGetMethodIdRef
 import gdscript.psi.GdVisitor
+import org.jetbrains.annotations.Unmodifiable
 
-class GdGetMethodIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdGetMethodIdRef, PolySymbolOwnReferencesHost {
+class GdGetMethodIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdGetMethodIdRef {
     fun accept(visitor: GdVisitor) {
         visitor.visitGetMethodIdRef(this)
     }
@@ -22,10 +23,15 @@ class GdGetMethodIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdGetMethodI
         else super.accept(visitor)
     }
 
-    override fun buildOwnReferences(builder: PolySymbolOwnReferences.Builder) {
-        val requireStatic = PsiTreeUtil.getStubOrPsiParentOfType(this, GdClassVarDeclTl::class.java)?.isStatic == true
-        builder.fromNameMatchQuery(METHOD, text) { symbol ->
-            !requireStatic || symbol.hasModifier(STATIC)
+    override fun getOwnReferences(): @Unmodifiable Collection<PsiSymbolReference> =
+        polySymbolOwnReferences(this) {
+            val requireStatic = PsiTreeUtil.getStubOrPsiParentOfType(
+                this@GdGetMethodIdRefImpl,
+                GdClassVarDeclTl::class.java
+            )?.isStatic == true
+            resolveFromNameMatchQuery(METHOD, text) { symbol ->
+                !requireStatic || symbol.hasModifier(STATIC)
+            }
         }
-    }
+
 }
