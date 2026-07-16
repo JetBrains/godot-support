b481d244fe13fc6646cd77f0bf4eb7a97ecff799
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-16 13:08:32 +0200

    RIDER-140303 GDScript: WIP on switching to PolySymbols - GdClassMemberReference removed and GDRefElements switched to providing own references
    GitOrigin-RevId: 7f59a2d24ee1c2b1058e30302be3edc41a16a5ce

--- gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiBindingPatternSymbol.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/psi/GdPsiBindingPatternSymbol.kt
@@ -5,6 +5,7 @@ import com.intellij.model.Pointer
 import com.intellij.polySymbols.PolySymbol
 import com.intellij.polySymbols.PolySymbolKind
 import com.intellij.psi.createSmartPointer
+import gdscript.GdKeywords
 import gdscript.polySymbols.GdPolySymbolKind
 import gdscript.polySymbols.completion.GdPolySymbolPriorities
 import gdscript.psi.GdVarNmi
@@ -31,6 +32,10 @@ class GdPsiBindingPatternSymbol(
     override val name: String get() = sourceElement.name
     override val declaringClassName: String get() = ""
     override val declaringClassId: String get() = ""
+    // Binding patterns have no type-hint syntax (`var x`, never `var x: int`) - the bound
+    // value's runtime type depends on which match branch executed, so this can't be inferred
+    // from the pattern itself the way GdCommonUtil.returnType infers other local kinds.
+    override val returnType: String get() = GdKeywords.VARIANT
 
     override val icon: Icon get() = GdScriptPluginIcons.GDScriptIcons.VAR_MARKER
     override val priority: PolySymbol.Priority get() = GdPolySymbolPriorities.LOCAL_USER_DEFINED
