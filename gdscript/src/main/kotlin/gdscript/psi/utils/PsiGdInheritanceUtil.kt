41dd964c3721c4498457e42db985c25d346ec47f
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-17 12:03:59 +0200

    RIDER-140303 GDScript: remove last remaining reference contributors except for GdResourceReference
    GitOrigin-RevId: 11ee6fc39a3d0002609f356effb3fcc628f55eda

--- gdscript/src/main/kotlin/gdscript/psi/utils/PsiGdInheritanceUtil.kt
+++ gdscript/src/main/kotlin/gdscript/psi/utils/PsiGdInheritanceUtil.kt
@@ -1,28 +1,13 @@
 package gdscript.psi.utils
 
-import com.intellij.psi.PsiFile
 import com.intellij.psi.util.elementType
-import gdscript.index.impl.GdClassNamingIndex
-import gdscript.index.impl.GdFileResIndex
 import gdscript.psi.GdInheritanceIdRef
 import gdscript.psi.GdTypes
-import gdscript.utils.VirtualFileUtil.getPsiFile
 
 object PsiGdInheritanceUtil {
 
     fun isClassName(inheritance: GdInheritanceIdRef): Boolean {
         return inheritance.firstChild.elementType == GdTypes.IDENTIFIER
-    }
-
-    fun getPsiFile(inheritance: GdInheritanceIdRef): PsiFile? {
-        val key = inheritance.text.trim('"', '\'')
-        if (key.startsWith("res://")) {
-            val virtual = GdFileResIndex.getFiles(key, inheritance.project).first()
-
-            return virtual.getPsiFile(inheritance.project)
-        }
-
-        return GdClassNamingIndex.INSTANCE.getGlobally(inheritance).firstOrNull()?.containingFile
     }
 
 }
