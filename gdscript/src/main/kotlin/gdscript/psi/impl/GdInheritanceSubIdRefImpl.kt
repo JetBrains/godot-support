36981d2907ac0a6533b5fc506b91918b831c39cf
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-17 12:03:59 +0200

    RIDER-140303 GDScript: remove last remaining reference contributors except for GdResourceReference
    GitOrigin-RevId: 11ee6fc39a3d0002609f356effb3fcc628f55eda

--- gdscript/src/main/kotlin/gdscript/psi/impl/GdInheritanceSubIdRefImpl.kt
+++ gdscript/src/main/kotlin/gdscript/psi/impl/GdInheritanceSubIdRefImpl.kt
@@ -1,9 +1,13 @@
 package gdscript.psi.impl
 
 import com.intellij.lang.ASTNode
+import com.intellij.model.psi.PsiSymbolReference
+import com.intellij.polySymbols.references.polySymbolOwnReferences
 import com.intellij.psi.PsiElementVisitor
+import gdscript.polySymbols.GdPolySymbolKind.INHERITANCE_SYMBOLS
 import gdscript.psi.GdInheritanceSubIdRef
 import gdscript.psi.GdVisitor
+import org.jetbrains.annotations.Unmodifiable
 
 class GdInheritanceSubIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdInheritanceSubIdRef {
     fun accept(visitor: GdVisitor) {
@@ -14,4 +18,9 @@ class GdInheritanceSubIdRefImpl(node: ASTNode) : GdRefElementImpl(node), GdInher
         if (visitor is GdVisitor) accept(visitor)
         else super.accept(visitor)
     }
+
+    override fun getOwnReferences(): @Unmodifiable Collection<PsiSymbolReference> =
+        polySymbolOwnReferences(this) {
+            resolveFromNameMatchQuery(INHERITANCE_SYMBOLS, text)
+        }
 }
