3e871ac1d10560442d44d5e4ad5be774fcbdfe36
Piotr Tomiak <piotr.tomiak@jetbrains.com>
2026-07-22 19:04:27 +0200

    RIDER-140303 GDScript: do not highlight self references
    GitOrigin-RevId: de0d7c020420d9e805e8c1cdaaf1ebf0b1c9607d

---
+++ gdscript/src/test/testData/gdscript/highlighting/autoloadOwnDeclarationColoring/global.gd
@@ -0,0 +1,7 @@
+extends <info descr="null"><symbolName descr="GD_ENGINE_TYPE">Node</symbolName></info>
+
+const <symbolName descr="GD_MEMBER">GREETING</symbolName> = "hello"
+
+
+func <info descr="null"><symbolName descr="GD_METHOD_CALL">_ready</symbolName></info>() -> void:
+	<symbolName descr="GD_GLOBAL_FUNCTION">print</symbolName>(<symbolName descr="GD_MEMBER">GREETING</symbolName>)
