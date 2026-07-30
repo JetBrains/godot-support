1330ecbf35b4a9663b4cc5aa4f4f9d5a5954a35d
Krystof Lacka <krystof.lacka@jetbrains.com>
2026-07-30 15:30:46 +0200

    fix RIDER-140942 Drag and drop with modifier of Godot scene nodes gives inconsistent results
    GitOrigin-RevId: e439cf19c652f95395a93cffa1deea7b282b5c56

--- gdscript/src/main/kotlin/GdScriptToolWindowManagerProjectActivity.kt
+++ gdscript/src/main/kotlin/GdScriptToolWindowManagerProjectActivity.kt
@@ -1,18 +1,22 @@
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
 import com.jetbrains.rd.util.threading.coroutines.launch
 import com.jetbrains.rider.godot.community.GdScriptProjectLifetimeService
 import com.jetbrains.rider.godot.community.utils.GodotCommunityUtil
 import tscn.toolWindow.TscnScenePreviewWindowFactory
+import tscn.toolWindow.model.ModifierTracker
 import tscn.toolWindow.model.SceneTreeEditorDropHandler
 
 class GdScriptToolWindowManagerProjectActivity : ProjectActivity {
     override suspend fun execute(project: Project) {
         GdScriptProjectLifetimeService.getLifetime(project).launch {
             GodotCommunityUtil.awaitGodotProject(project)
+            if(!ApplicationManager.getApplication().isHeadlessEnvironment) {
+                ModifierTracker.getInstance()
+            }
             TscnScenePreviewWindowFactory.makeAvailable(project)
             SceneTreeEditorDropHandler.installIntoExistingEditors(project)
         }
     }
 }
-
