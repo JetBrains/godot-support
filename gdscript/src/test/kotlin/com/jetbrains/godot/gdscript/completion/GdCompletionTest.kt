d889194c5d7cc6b046cc90b3953945538297cf95
Martin Tomanek <martin.tomanek@jetbrains.com>
2026-08-08 16:02:46 +0200

    fix RIDER-141543 Completion for % UniqueName nodes in gdscript
    GitOrigin-RevId: 436ad8fca5646ab3f22ea098f2467661490b14fc

--- gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/completion/GdCompletionTest.kt
+++ gdscript/src/test/kotlin/com/jetbrains/godot/gdscript/completion/GdCompletionTest.kt
@@ -1,5 +1,8 @@
 package com.jetbrains.godot.gdscript.completion
 
+import com.intellij.codeInsight.lookup.Lookup
+import com.intellij.codeInsight.lookup.LookupElementPresentation
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture
 import com.jetbrains.godot.gdscript.GdTestCaseWithSdk
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -90,5 +93,100 @@ class GdCompletionTest : GdTestCaseWithSdk("completion") {
             it.displayText?.startsWith("action_") == true
         }
     )
+
+    @Test
+    fun testQuotedUniqueNameLookup() = doUniqueNamesTest("%\"<caret>\"") {
+        val items = lookupElementStrings!!
+        assertContainsElements(items, "%\"HealthBar\"", "%\"Score Label\"", "%\"Main-Menu\"")
+        assertDoesntContain(items, "%\"Plain\"", "%\"Main\"", "\$Plain")
+    }
+
+    @Test
+    fun testQuotedUniqueNameInsertsWithoutDuplicateQuote() = doUniqueNamesTest("%\"<caret>\"") {
+        val lookup = lookup!!
+        lookup.currentItem = lookup.items.single { it.lookupString == "%\"Score Label\"" }
+        finishLookup(Lookup.NORMAL_SELECT_CHAR)
+
+        assertEquals(script("%\"Score Label\""), editor.document.text)
+    }
+
+    /**
+     * The unquoted `%<caret>` form stays unquoted for names Godot can parse as an identifier and
+     * switches to `%"..."` for the ones it cannot.
+     */
+    @Test
+    fun testUnquotedUniqueNameLookupQuotesOnlyWhenRequired() = doUniqueNamesTest("%<caret>") {
+        val items = lookupElementStrings!!
+        assertContainsElements(items, "%HealthBar", "%\"Score Label\"", "%\"Main-Menu\"")
+        assertDoesntContain(items, "%Score Label", "%Main-Menu", "%Plain")
+    }
+
+    @Test
+    fun testInstancedNodesAreOffered() = doUniqueNamesTest("%\"<caret>\"") {
+        val items = lookupElementStrings!!
+        assertContainsElements(items, "%\"Player\"")
+        assertDoesntContain(items, "%\"PlayerRoot\"")
+
+        val player = lookup!!.items.single { it.lookupString == "%\"Player\"" }
+        assertEquals("CharacterBody2D", LookupElementPresentation.renderElement(player).typeText)
+    }
+
+    @Test
+    fun testInstancedNodeChildrenAreOffered() = doUniqueNamesTest("\$<caret>") {
+        assertContainsElements(lookupElementStrings!!, "\$Player", "\$Player/Sprite")
+    }
+
+    @Test
+    fun testInstancedNodeUniqueChildrenAreNotOffered() = doUniqueNamesTest("%\"<caret>\"") {
+        val items = lookupElementStrings!!
+        assertDoesntContain(items, "%\"PlayerUnique\"")
+    }
+
+    @Test
+    fun testTscnSelfRecursion() = doSelfTscnRecursionTest("\$<caret>") {
+        assertContainsElements(lookupElementStrings!!, "\$Main/SelfRec", "\$Main")
+    }
+
+    @Test
+    fun testTscnMutualRecursion() = doMutualTscnRecursionTest("\$<caret>") {
+        // "\$MainRec1/Rec1/Rec2" is not there, because the recursion guard fires on it
+        assertContainsElements(lookupElementStrings!!, "\$MainRec1/Rec1", "\$MainRec1")
+    }
+
+    private fun script(expression: String): String =
+        "extends Node2D\n\nfunc _ready():\n\tvar node = $expression\n"
+
+    private fun doUniqueNamesTest(expression: String, check: CodeInsightTestFixture.() -> Unit) =
+        doConfiguredTest(
+            dir = true,
+            dirName = "uniqueNames",
+            configureFileName = "unique_names.gd",
+            fileContents = script(expression),
+        ) {
+            completeBasic()
+            check()
+        }
+
+    private fun doSelfTscnRecursionTest(expression: String, check: CodeInsightTestFixture.() -> Unit) =
+        doConfiguredTest(
+            dir = true,
+            dirName = "tscnSelfRecursion",
+            configureFileName = "tscn_recursion.gd",
+            fileContents = script(expression),
+        ) {
+            completeBasic()
+            check()
+        }
+
+    private fun doMutualTscnRecursionTest(expression: String, check: CodeInsightTestFixture.() -> Unit) =
+        doConfiguredTest(
+            dir = true,
+            dirName = "tscnMutualRecursion",
+            configureFileName = "tscn_recursion.gd",
+            fileContents = script(expression),
+        ) {
+            completeBasic()
+            check()
+        }
 
 }
