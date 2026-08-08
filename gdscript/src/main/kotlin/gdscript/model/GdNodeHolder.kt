4a805eb570fe3723b1a46db9bb5e169ed21c1d51
Martin Tomanek <martin.tomanek@jetbrains.com>
2026-08-08 16:02:46 +0200

    fix RIDER-141543 Completion for % UniqueName nodes in gdscript
    GitOrigin-RevId: 436ad8fca5646ab3f22ea098f2467661490b14fc

--- gdscript/src/main/kotlin/gdscript/model/GdNodeHolder.kt
+++ gdscript/src/main/kotlin/gdscript/model/GdNodeHolder.kt
@@ -2,8 +2,10 @@ package gdscript.model
 
 import com.intellij.codeInsight.lookup.LookupElement
 import gdscript.completion.GdLookup
+import gdscript.completion.handler.GdQuotedNodePathInsertHandler
 import gdscript.completion.handler.GdReplaceInsertHandler
-import gdscript.utils.StringUtil.camelToSnakeCase
+import gdscript.psi.utils.GdNodeUtil.nodeNameToIdentifier
+import gdscript.psi.utils.GdNodeUtil.quoteIfNeeded
 import tscn.psi.TscnNodeHeader
 
 /**
@@ -17,51 +19,76 @@ data class GdNodeHolder(
         val readableNodePath: String,
         val script: String? = null,
         val nodePath: String? = null,
+        /** Usually [element]'s own type, but an instanced node from scene takes it from its sub scene root. */
+        val type: String = element.type,
 ) {
 
+    /**
+     * Text to insert for the unique name: `%Name`, or `%"Name"` when the node name does not fit
+     * the unquoted node path alphabet. [uniqueId] itself stays unquoted because it is the key
+     * [gdscript.psi.utils.GdNodeUtil.findNode] resolves against.
+     */
+    private val uniqueNamepath: String?
+        get() = if (uniqueId == null) null else "%${quoteIfNeeded(element.name)}"
+
     fun lookups(): List<LookupElement> {
+        val uniqueRef = this.uniqueNamepath
+
         return listOfNotNull(
                 GdLookup.create(
                         readableNodePath,
                         color = GdLookup.COLOR_RESOURCE,
                         priority = GdLookup.REMOTE_DEFINED,
-                        typed = element.type,
+                        typed = type,
                         tail = extraInfo,
-                        handler = GdReplaceInsertHandler(uniqueId ?: relativePath),
+                        handler = GdReplaceInsertHandler(uniqueRef ?: relativePath),
                 ),
-                if (uniqueId != null)
+                if (uniqueRef != null)
                     GdLookup.create(
-                            uniqueId,
+                            uniqueRef,
                             color = GdLookup.COLOR_RESOURCE,
                             priority = GdLookup.REMOTE_DEFINED,
-                            typed = element.type,
-                            handler = GdReplaceInsertHandler(uniqueId),
+                            typed = type,
+                            handler = GdReplaceInsertHandler(uniqueRef),
                     ) else null
         )
     }
 
-    fun variable_lookups(): List<LookupElement> {
-        var type = element.type
-        if (type.isNotBlank()) {
-            type = ": $type"
-        }
+    /** Lookups for the quoted unique name context `%"<caret>"`. */
+    fun quotedUniqueLookups(): List<LookupElement> {
+        if (uniqueId == null) return emptyList()
+
+        return listOf(
+                GdLookup.create(
+                        "%\"${element.name}\"",
+                        color = GdLookup.COLOR_RESOURCE,
+                        priority = GdLookup.REMOTE_DEFINED,
+                        typed = type,
+                        handler = GdQuotedNodePathInsertHandler,
+                )
+        )
+    }
+
+    fun variableLookups(): List<LookupElement> {
+        val typeHint = if (type.isNotBlank()) ": $type" else ""
+        val uniqueRef = this.uniqueNamepath
 
         return listOfNotNull(
                 GdLookup.create(
                         readableNodePath,
                         color = GdLookup.COLOR_RESOURCE,
                         priority = GdLookup.REMOTE_DEFINED,
-                        typed = element.type,
+                        typed = type,
                         tail = extraInfo,
-                        handler = GdReplaceInsertHandler("@onready var ${element.name.camelToSnakeCase()}$type = ${uniqueId ?: relativePath}"),
+                        handler = GdReplaceInsertHandler("@onready var ${nodeNameToIdentifier(element.name)}$typeHint = ${uniqueRef ?: relativePath}"),
                 ),
-                if (uniqueId != null)
+                if (uniqueRef != null)
                     GdLookup.create(
-                            uniqueId,
+                            uniqueRef,
                             color = GdLookup.COLOR_RESOURCE,
                             priority = GdLookup.REMOTE_DEFINED,
-                            typed = element.type,
-                            handler = GdReplaceInsertHandler("@onready var ${element.name.camelToSnakeCase()}$type = $uniqueId"),
+                            typed = type,
+                            handler = GdReplaceInsertHandler("@onready var ${nodeNameToIdentifier(element.name)}$typeHint = $uniqueRef"),
                     ) else null
         )
     }
