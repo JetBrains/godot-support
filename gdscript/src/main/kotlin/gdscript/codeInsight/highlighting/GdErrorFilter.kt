1b9889308d8cd16a2c2a92615b50360b4083c620
Martin Tomanek <martin.tomanek@jetbrains.com>
2026-08-11 17:10:32 +0200

    fix RIDER-141759 Godot: GDScript completion in the debug immediate window
    GitOrigin-RevId: 375d50418edd84afb29de2dc3ae6ca3259865ea8

--- gdscript/src/main/kotlin/gdscript/codeInsight/highlighting/GdErrorFilter.kt
+++ gdscript/src/main/kotlin/gdscript/codeInsight/highlighting/GdErrorFilter.kt
@@ -4,9 +4,11 @@ import com.intellij.codeInsight.highlighting.HighlightErrorFilter
 import com.intellij.psi.PsiErrorElement
 import gdscript.lsp.GodotLspRunningStatusProvider
 import gdscript.psi.GdFile
+import gdscript.psi.GdPsiCodeFragment
 
 class GdErrorFilter : HighlightErrorFilter() {
     override fun shouldHighlightErrorElement(el: PsiErrorElement): Boolean {
+        if (el.containingFile is GdPsiCodeFragment) return true // Code fragments don't use LSP, so we need our annotations
         if (el.containingFile is GdFile && GodotLspRunningStatusProvider.isLspRunning(el.project))
             return false
         return true
