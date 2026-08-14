8a7ac73aa2c4000f8018f527bee6c6b58e6c0392
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2026-08-14 18:38:33 +0200

    [i18n] IJPL-203622 Replace DynamicBundle inheritance with composition using instance delegation
    GitOrigin-RevId: ee7f7086238d36eb343048828216eb94cf94f0c2

--- rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotPluginBundle.kt
+++ rider/src/main/kotlin/com/jetbrains/rider/plugins/godot/GodotPluginBundle.kt
@@ -5,18 +5,16 @@ import org.jetbrains.annotations.Nls
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
-class GodotPluginBundle : DynamicBundle(BUNDLE) {
-    companion object {
-        @NonNls
-        private const val BUNDLE = "messages.GodotPluginBundle"
-        private val INSTANCE: GodotPluginBundle = GodotPluginBundle()
+object GodotPluginBundle {
+  @NonNls
+  private const val BUNDLE = "messages.GodotPluginBundle"
+  private val instance = DynamicBundle(GodotPluginBundle::class.java, BUNDLE)
 
-        @Nls
-        fun message(
-            @PropertyKey(resourceBundle = BUNDLE) key: String,
-            vararg params: Any
-        ): String {
-            return INSTANCE.getMessage(key, *params)
-        }
-    }
+  @Nls
+  fun message(
+    @PropertyKey(resourceBundle = BUNDLE) key: String,
+    vararg params: Any
+  ): String {
+    return instance.getMessage(key, *params)
+  }
 }
