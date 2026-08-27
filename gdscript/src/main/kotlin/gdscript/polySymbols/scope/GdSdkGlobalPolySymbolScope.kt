2870b6b2e6a437fba4076f8ca07fc705e423e1b5
Ivan Shakhov <ivan.shakhov@jetbrains.com>
2026-08-27 17:05:59 +0000

    fix RIDER-142322, fix RIDER-142351, fix RIDER-142362
    Merge-request: IJ-MR-220254
    Merged-by: Ivan Šachov <Ivan.Shakhov@jetbrains.com>
    
    GitOrigin-RevId: 52c09d96f97cd7e18dfc83d478be438d032215f6

--- gdscript/src/main/kotlin/gdscript/polySymbols/scope/GdSdkGlobalPolySymbolScope.kt
+++ gdscript/src/main/kotlin/gdscript/polySymbols/scope/GdSdkGlobalPolySymbolScope.kt
@@ -3,6 +3,7 @@ package gdscript.polySymbols.scope
 import com.intellij.openapi.project.Project
 import com.intellij.polySymbols.query.PolySymbolScope
 import com.intellij.polySymbols.query.polySymbolScopeCached
+import gdscript.library.GdSdkDocsTracker
 import gdscript.library.GdSdkFilesProvider
 import gdscript.polySymbols.GdPolySymbolKind
 import gdscript.polySymbols.GdPolySymbolsConstants
@@ -14,7 +15,9 @@ import gdscript.polySymbols.sdk.GdSdkSignalSymbol
 import gdscript.polySymbols.sdk.xml.GdSdkXmlParser
 
 /**
- * Scope for global symbols inherent to the language defined in GDScript core SDK files.
+ * Scope for global symbols inherent to the language defined in GDScript core SDK files, plus the generated
+ * `@GDExtensionScope.xml` that declares the GDExtension singletons as global variables (see
+ * [gdscript.library.GdGlobalSingletonsDocWriter]) - hence all SDK files are looked through, not only the core ones.
  */
 fun gdSdkGlobalPolySymbolScope(project: Project): PolySymbolScope =
     polySymbolScopeCached(project) {
@@ -26,10 +29,10 @@ fun gdSdkGlobalPolySymbolScope(project: Project): PolySymbolScope =
             GdPolySymbolKind.SIGNAL,
         )
         initialize {
-            cacheDependencies(GdSdkSymbolsModificationTracker.getInstance(project))
+            cacheDependencies(GdSdkDocsTracker.getInstance(project))
 
             // TODO optimize this, dont look through all files, only GDSCRIPT_GLOBAL_SCOPE_FILE_NAMES
-            GdSdkFilesProvider.getInstance(project).getAllCoreSdkFiles().forEach { file ->
+            GdSdkFilesProvider.getInstance(project).getAllSdkFiles().forEach { file ->
                 if (GdPolySymbolsConstants.GLOBAL_CLASSES.contains(file.nameWithoutExtension)) {
                     val classData = GdSdkXmlParser.parseClass(file) ?: return@forEach
                     // classData.constructors is always empty for GLOBAL_CLASSES files (global/utility
