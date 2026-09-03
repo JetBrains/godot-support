a0831504126910f976e7bd4ab70586ec62028c3c
Krystof Lacka <krystof.lacka@jetbrains.com>
2026-09-03 11:35:10 +0000

    fix RIDER-137845: [GodotEditor addon] Protocol connection between Rider frontend and GodotEditor
    Merge-request: IJ-MR-213732
    Merged-by: Kryštof Lacka <krystof.lacka@jetbrains.com>
    
    GitOrigin-RevId: eb8f46efaa146a85e4dd5a567623c5b8886e6711

---
+++ godot-rd/src/register_types.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <godot_cpp/godot.hpp>
+
+void initialize_gdextension_types(godot::ModuleInitializationLevel p_level);
+void uninitialize_gdextension_types(godot::ModuleInitializationLevel p_level);
